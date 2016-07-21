d2e61adcc25f6af616cb68cfd80240f4f15746a1
Alexander Baratynskiy <Alexander.Baratynskiy@Labs.IntelliJ.Net>
2016-07-21 15:14:44 +0300

    Updated Maven helper

--- src/main/java/org/black/kotlin/projectsextensions/maven/MavenHelper.java
+++ src/main/java/org/black/kotlin/projectsextensions/maven/MavenHelper.java
@@ -1,6 +1,8 @@
 package org.black.kotlin.projectsextensions.maven;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import org.jetbrains.annotations.Nullable;
 import org.netbeans.modules.maven.NbMavenProjectFactory;
 import org.netbeans.modules.maven.NbMavenProjectImpl;
@@ -53,4 +55,46 @@ public class MavenHelper {
         }
         return false;
     }
+    
+    private static FileObject getParentProjectDirectory(FileObject proj) {
+        if (projectFactory.isProject(proj.getParent())) {
+            FileObject parent = getParentProjectDirectory(proj.getParent());
+            
+            if (parent != null) {
+                return parent;
+            }
+            return proj.getParent();
+        }
+        
+        return null;
+    }
+    
+    public static NbMavenProjectImpl getMainParent(NbMavenProjectImpl proj) throws IOException {
+        NbMavenProjectImpl parent = getMavenProject(getParentProjectDirectory(proj.getProjectDirectory()));
+        
+        return parent != null ? parent : proj;
+    }
+    
+    public static List<FileObject> getAllChildrenSrcDirectoriesOfProject(NbMavenProjectImpl project) {
+        List modules = project.getOriginalMavenProject().getModules();
+        List<FileObject> srcDirs = new ArrayList<FileObject>();
+        
+        for (Object module : modules) {
+            if (projectFactory.isProject(project.getProjectDirectory().getFileObject((String) module))) {
+                try {
+                    NbMavenProjectImpl child = MavenHelper.getMavenProject(project.getProjectDirectory().getFileObject((String) module));
+                    if (isModuled(child)){
+                        srcDirs.addAll(getAllChildrenSrcDirectoriesOfProject(child));
+                    } else {
+                        srcDirs.add(child.getProjectDirectory().getFileObject("src"));
+                    }
+                } catch (IOException ex) {
+                    Exceptions.printStackTrace(ex);
+                }
+            }
+        }
+        
+        return srcDirs;
+    }
+    
 }

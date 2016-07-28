08d1baca30d3de1d3d57b25a3f2ed5021c5f74da
Alexander Baratynskiy <Alexander.Baratynskiy@Labs.IntelliJ.Net>
2016-07-28 13:12:02 +0300

    One more minor refactoring

--- src/test/java/highlighting/TokenTypeTest.java
+++ src/test/java/highlighting/TokenTypeTest.java
@@ -1,44 +1,23 @@
 package highlighting;
 
 import org.black.kotlin.highlighter.TokenType;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.netbeans.junit.NbTestCase;
 
 /**
  *
  * @author Александр
  */
-public class TokenTypeTest {
-    
-    public TokenTypeTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
+public class TokenTypeTest extends NbTestCase{
 
+    public TokenTypeTest() {
+        super("Token type test");
+    }
     /**
      * Test of getId method, of class TokenType.
      */
     @Test
     public void testGetId() {
-        System.out.println("getId");
         assertEquals(1, TokenType.IDENTIFIER.getId());
         assertEquals(0, TokenType.KEYWORD.getId());
         assertEquals(6, TokenType.WHITESPACE.getId());

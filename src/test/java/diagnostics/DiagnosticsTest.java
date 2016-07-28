08a7525d1fe0dd893ab4fbddca5823d81d6f8985
Alexander Baratynskiy <Alexander.Baratynskiy@Labs.IntelliJ.Net>
2016-07-28 13:07:20 +0300

    Diagnostics tests refactoring

--- src/test/java/diagnostics/DiagnosticsTest.java
+++ src/test/java/diagnostics/DiagnosticsTest.java
@@ -1,6 +1,9 @@
 package diagnostics;
 
 import com.intellij.psi.PsiErrorElement;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import org.junit.Test;
 import org.netbeans.api.project.Project;
 import org.netbeans.junit.NbTestCase;
@@ -15,6 +18,7 @@ import org.jetbrains.kotlin.resolve.AnalyzingUtils;
 import org.openide.filesystems.FileObject;
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
+import kotlin.Pair;
 
 /**
  *
@@ -26,7 +30,7 @@ public class DiagnosticsTest extends NbTestCase {
     private final FileObject diagnosticsDir;
     
     public DiagnosticsTest() {
-        super("First test");
+        super("Diagnostics test");
         project = JavaProject.INSTANCE.getJavaProject();
         diagnosticsDir = project.getProjectDirectory().
                 getFileObject("src").getFileObject("diagnostics");  
@@ -42,6 +46,66 @@ public class DiagnosticsTest extends NbTestCase {
         return KotlinAnalyzer.analyzeFile(project, ktFile);
     }
     
+    private void doTest(String fileName, int numberOfDiagnostics, 
+            List<Pair<Integer,Integer>> diagnosticsRanges, 
+            List<Severity> diagnosticsSeverity, int numberOfSyntaxErrors,
+            List<Pair<Integer, Integer>> syntaxErrorsRanges) {
+        
+        AnalysisResultWithProvider result = getAnalysisResult(fileName);
+        FileObject fileToAnalyze = diagnosticsDir.getFileObject(fileName);
+        KtFile ktFile = ProjectUtils.getKtFile(fileToAnalyze);
+        
+        Collection<Diagnostic> diagnostics = 
+                result.getAnalysisResult().getBindingContext().getDiagnostics().all();
+        Collection<PsiErrorElement> syntaxErrors = AnalyzingUtils.getSyntaxErrorRanges(ktFile);
+        
+        assertEquals(diagnostics.size(), numberOfDiagnostics);
+        assertEquals(syntaxErrors.size(), numberOfSyntaxErrors);
+        
+        if (numberOfDiagnostics > 0) {
+            int i = 0;
+            for (Diagnostic diagnostic : diagnostics) {
+                assertEquals(diagnosticsSeverity.get(i), diagnostic.getSeverity());
+                
+                Integer startPosition = diagnostic.getTextRanges().get(0).getStartOffset();
+                Integer endPosition = diagnostic.getTextRanges().get(0).getEndOffset();
+                
+                assertEquals(startPosition, diagnosticsRanges.get(i).getFirst());
+                assertEquals(endPosition, diagnosticsRanges.get(i).getSecond());
+                
+                i++;
+            }
+        }
+        
+        if (numberOfSyntaxErrors > 0) {
+            int i = 0;
+            for (PsiErrorElement syntaxError : syntaxErrors) {
+                Integer startPosition = syntaxError.getTextRange().getStartOffset();
+                Integer endPosition = syntaxError.getTextRange().getEndOffset();
+        
+                assertEquals(startPosition, syntaxErrorsRanges.get(i).getFirst());
+                assertEquals(endPosition, syntaxErrorsRanges.get(i).getSecond());
+            }
+        }
+    }
+    
+    private void doTest(String fileName, int numberOfDiagnostics, 
+            List<Pair<Integer,Integer>> diagnosticsRanges, 
+            List<Severity> diagnosticsSeverity) {
+        doTest(fileName, numberOfDiagnostics, diagnosticsRanges, diagnosticsSeverity,
+                0, new ArrayList<Pair<Integer, Integer>>());
+    }
+    
+    private void doTest(String fileName, int numberOfSyntaxErrors,
+            List<Pair<Integer, Integer>> syntaxErrorsRanges) {
+        doTest(fileName, 0, new ArrayList<Pair<Integer, Integer>>(), 
+                null, numberOfSyntaxErrors, syntaxErrorsRanges);
+    }
+    
+    private void doTest(String fileName) {
+        doTest(fileName, 0, new ArrayList<Pair<Integer, Integer>>());
+    }
+    
     @Test
     public void testProjectCreation() {
         assertNotNull(project);
@@ -55,41 +119,27 @@ public class DiagnosticsTest extends NbTestCase {
     
     @Test
     public void testParameterIsNeverUsedWarning(){
-        AnalysisResultWithProvider result = getAnalysisResult("parameterIsNeverUsed.kt");
+        List<Pair<Integer,Integer>> diagnosticsRanges = 
+                new ArrayList<Pair<Integer,Integer>>();
+        diagnosticsRanges.add(new Pair<Integer, Integer>(30,33));
         
-        Diagnostic diagnostic = 
-                result.getAnalysisResult().getBindingContext().getDiagnostics().iterator().next();
+        List<Severity> severityList = new ArrayList<Severity>();
+        severityList.add(Severity.WARNING);
         
-        assertEquals(Severity.WARNING, diagnostic.getSeverity());
-        
-        int startPosition = diagnostic.getTextRanges().get(0).getStartOffset();
-        int endPosition = diagnostic.getTextRanges().get(0).getEndOffset();
-        
-        assertEquals(startPosition, 30);
-        assertEquals(endPosition, 33);
+        doTest("parameterIsNeverUsed.kt", 1, diagnosticsRanges, severityList);
     }
     
     @Test
     public void testExpectingATopLevelDeclarationError(){
-        AnalysisResultWithProvider result = getAnalysisResult("expectingATopLevelDeclaration.kt");
-        FileObject fileToAnalyze = diagnosticsDir.getFileObject("expectingATopLevelDeclaration.kt");
-        KtFile ktFile = ProjectUtils.getKtFile(fileToAnalyze);
-        PsiErrorElement psiError = AnalyzingUtils.getSyntaxErrorRanges(ktFile).get(0);
+        List<Pair<Integer, Integer>> errorsRanges = 
+                new ArrayList<Pair<Integer, Integer>>();
+        errorsRanges.add(new Pair<Integer, Integer>(21,31));
         
-        assertNotNull(psiError);
-        
-        int startPosition = psiError.getTextRange().getStartOffset();
-        int endPosition = psiError.getTextRange().getEndOffset();
-        
-        assertEquals(21,startPosition);
-        assertEquals(31,endPosition);
-        assertTrue(result.getAnalysisResult().getBindingContext().getDiagnostics().isEmpty());
+        doTest("expectingATopLevelDeclaration.kt", 1, errorsRanges);
     }
     
     @Test
     public void testNoTypeMismatch(){
-        AnalysisResultWithProvider result = getAnalysisResult("checkNoTypeMismatch.kt");
-        
-        assertTrue(result.getAnalysisResult().getBindingContext().getDiagnostics().isEmpty());
+        doTest("checkNoTypeMismatch.kt");
     }
 }

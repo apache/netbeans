e500c8109df8a872d8031d4cbd032bf7a7a0acf7
Alexander Baratynskiy <Alexander.Baratynskiy@jetbrains.com>
2016-07-29 16:50:32 +0300

    KotlinTypeSearchProvider

---
+++ src/main/java/org/black/kotlin/navigation/JavaKotlinTypeSearchProvider.java
@@ -0,0 +1,17 @@
+package org.black.kotlin.navigation;
+
+import org.netbeans.modules.jumpto.quicksearch.JavaTypeSearchProvider;
+import org.netbeans.spi.quicksearch.SearchRequest;
+import org.netbeans.spi.quicksearch.SearchResponse;
+
+/**
+ *
+ * @author Alexander.Baratynski
+ */
+public class JavaKotlinTypeSearchProvider extends JavaTypeSearchProvider {
+
+    @Override
+    public void evaluate(SearchRequest request, SearchResponse response) {
+    }
+    
+}

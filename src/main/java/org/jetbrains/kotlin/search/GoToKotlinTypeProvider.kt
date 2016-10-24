df516fee9641a980047ad4b38ea0c5ac4d54a13e
Baratynskiy <Alexander.Baratynskiy@jetbrains.com>
2016-10-24 14:50:50 +0300

    GoToKotlinTypeProvider.kt

---
+++ src/main/java/org/jetbrains/kotlin/search/GoToKotlinTypeProvider.kt
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright 2000-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *******************************************************************************/
+package org.jetbrains.kotlin.search
+
+import java.io.File
+import javax.swing.text.StyledDocument
+import org.jetbrains.kotlin.navigation.NavigationUtil
+import org.jetbrains.kotlin.psi.KtDeclaration
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.utils.ProjectUtils
+import org.netbeans.api.project.Project
+import org.netbeans.api.project.ui.OpenProjects
+import org.netbeans.spi.quicksearch.SearchProvider
+import org.netbeans.spi.quicksearch.SearchRequest
+import org.netbeans.spi.quicksearch.SearchResponse
+import org.openide.filesystems.FileObject
+import org.openide.filesystems.FileUtil
+
+class GoToKotlinTypeProvider : SearchProvider {
+
+    override fun evaluate(request: SearchRequest, response: SearchResponse) {
+        OpenProjects.getDefault().getOpenProjects()
+                .flatMap { KotlinTypeSearcher.searchDeclaration(it, request.text) }
+                .forEach {
+                    val file = it.first
+                    it.second.forEach {
+                        val html = "${it.name} (${file.packageFqName.asString()})"
+                        response.addResult(action@{
+                            val f = File(file.virtualFile.path)
+                            val fo = FileUtil.toFileObject(f) ?: return@action
+
+                            val doc = ProjectUtils.getDocumentFromFileObject(fo) ?: return@action
+                            NavigationUtil.openFileAtOffset(doc, it.textRange.startOffset)
+                        }, html)
+                    }
+                }
+    }
+}

0e5c7f9318c6f4d1205e5b4fedf0a06b5afdb009
Baratynskiy <Alexander.Baratynskiy@jetbrains.com>
2016-11-28 16:10:40 +0300

    KotlinIndexer commented code deleted

--- src/main/java/org/jetbrains/kotlin/indexer/KotlinIndexer.java
+++ src/main/java/org/jetbrains/kotlin/indexer/KotlinIndexer.java
@@ -18,16 +18,10 @@
  */
 package org.jetbrains.kotlin.indexer;
 
-import org.jetbrains.kotlin.analyzer.AnalysisResult;
-import org.jetbrains.kotlin.diagnostics.netbeans.parser.KotlinParserResult;
-import org.jetbrains.kotlin.filesystem.lightclasses.KotlinLightClassGeneration;
-import org.jetbrains.kotlin.resolve.AnalysisResultWithProvider;
-import org.netbeans.api.project.Project;
 import org.netbeans.modules.parsing.spi.Parser;
 import org.netbeans.modules.parsing.spi.indexing.Context;
 import org.netbeans.modules.parsing.spi.indexing.EmbeddingIndexer;
 import org.netbeans.modules.parsing.spi.indexing.Indexable;
-import org.openide.filesystems.FileObject;
 
 /**
  *
@@ -35,32 +29,8 @@ import org.openide.filesystems.FileObject;
  */
 public class KotlinIndexer extends EmbeddingIndexer {
 
-    private static final Object LOCK = new Object(){};
-    
     @Override
     protected void index(Indexable indexable, Parser.Result parserResult, Context context) {
-//        final KotlinParserResult result = (KotlinParserResult) parserResult;
-//        AnalysisResultWithProvider analysisResult = result.getAnalysisResult();
-//        
-//        if (analysisResult == null) {
-//            return;
-//        }
-//        
-//        final FileObject fo = result.getSnapshot().getSource().getFileObject();
-//        final AnalysisResult res = analysisResult.getAnalysisResult();
-//        Thread thread = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                generateLightClass(fo, result.getProject(), res);
-//            }
-//        });
-//        thread.start();
     }
-    
-//    private void generateLightClass(FileObject fo, Project project, AnalysisResult analysisResult) {
-//        synchronized (LOCK) {
-//            KotlinLightClassGeneration.INSTANCE.generate(fo, project, analysisResult);
-//        }
-//    }
     
 }

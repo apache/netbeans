fea23d1d48e7ad9087f26566d890b4e0fab7587a
baratynskiy <baratynskiy@gmail.com>
2017-01-19 16:05:04 +0300

    KotlinRenameRefactoring.java -> .kt

---
+++ src/main/java/org/jetbrains/kotlin/refactorings/rename/KotlinRenameRefactoring.kt
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright 2000-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *******************************************************************************/
+package org.jetbrains.kotlin.refactorings.rename
+
+import com.intellij.psi.PsiElement
+import javax.swing.text.StyledDocument
+import org.jetbrains.kotlin.utils.ProjectUtils
+import org.jetbrains.kotlin.project.KotlinSources
+import org.netbeans.modules.parsing.api.indexing.IndexingManager
+import org.netbeans.modules.refactoring.api.Problem
+import org.netbeans.modules.refactoring.api.RenameRefactoring
+import org.netbeans.modules.refactoring.spi.ProgressProviderAdapter
+import org.netbeans.modules.refactoring.spi.RefactoringElementsBag
+import org.netbeans.modules.refactoring.spi.RefactoringPlugin
+
+class KotlinRenameRefactoring(val refactoring: RenameRefactoring) : ProgressProviderAdapter(), RefactoringPlugin {
+    
+    override fun prepare(bag: RefactoringElementsBag): Problem? {
+        val newName = refactoring.newName
+        val fo = ProjectUtils.getFileObjectForDocument(refactoring.refactoringSource.lookup(StyledDocument::class.java)) ?: return null
+        val psi = refactoring.refactoringSource.lookup(PsiElement::class.java)
+        
+        val renameMap = getRenameRefactoringMap(fo, psi, newName)
+        bag.registerTransaction(getTransaction(renameMap, newName, psi.text))
+        bag.session.doRefactoring(true)
+        
+        val project = ProjectUtils.getKotlinProjectForFileObject(fo) ?: return null
+        KotlinSources(project).getAllKtFiles().forEach {
+            IndexingManager.getDefault().refreshAllIndices(it)
+        }
+        
+        return null
+    }
+
+    override fun checkParameters() = null
+
+    override fun preCheck() = null
+
+    override fun fastCheckParameters() = null
+
+    override fun cancelRequest() {}
+}

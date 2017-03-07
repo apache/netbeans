8e39c709a131ec4c12fc03459d336e97c8c1f488
baratynskiy <baratynskiy@gmail.com>
2017-03-07 12:19:27 +0300

    Remove unused import fix

--- src/main/java/org/jetbrains/kotlin/hints/UnusedImportsComputer.kt
+++ src/main/java/org/jetbrains/kotlin/hints/UnusedImportsComputer.kt
@@ -23,54 +23,71 @@ import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.descriptors.*
 import org.netbeans.modules.csl.api.Hint
+import org.netbeans.modules.csl.api.HintFix
 import org.netbeans.modules.csl.api.HintSeverity
 import org.netbeans.modules.csl.api.OffsetRange
 
 class UnusedImportsComputer(private val parserResult: KotlinParserResult) : KtVisitor<Unit, Any?>() {
-    
+
     private val ktFile = parserResult.ktFile
     private val context = parserResult.analysisResult!!.analysisResult.bindingContext
     private val usedClasses = hashSetOf<ClassDescriptor>()
-    
+
     fun getUnusedImports(): List<Hint> {
         ktFile.accept(this)
         val unusedImports = hashSetOf<KtImportDirective>()
-        
+
         ktFile.importDirectives
                 .forEach {
                     val ref = PsiTreeUtil.findChildrenOfType(it, KtReferenceExpression::class.java).lastOrNull()
                     val target = context[BindingContext.REFERENCE_TARGET, ref]
                     if (target is ClassDescriptor && target !in usedClasses) unusedImports.add(it)
                 }
-        
+
         return unusedImports.map {
-            Hint(KotlinRule(HintSeverity.WARNING), 
+            Hint(KotlinRule(HintSeverity.WARNING),
                     "Unused import: ${it.importedFqName}",
                     parserResult.snapshot.source.fileObject,
                     OffsetRange(it.textRange.startOffset, it.textRange.endOffset),
-                    null, 
+                    listOf(UnusedHintFix(parserResult, it)),
                     Priorities.HINT_PRIORITY)
         }
     }
-    
+
     override fun visitKtFile(ktFile: KtFile, data: Any?) {
         ktFile.acceptChildren(this)
     }
-    
+
     override fun visitKtElement(element: KtElement, data: Any?) {
         element.acceptChildren(this)
     }
-    
+
     override fun visitImportDirective(importDirective: KtImportDirective, data: Any?) {}
-    
+
     override fun visitReferenceExpression(expression: KtReferenceExpression, data: Any?) {
-        val reference = context[BindingContext.REFERENCE_TARGET, expression]?.let { 
+        val reference = context[BindingContext.REFERENCE_TARGET, expression]?.let {
             if (it is ClassDescriptor && !it.isCompanionObject) it else it.containingDeclaration
         }
-        
+
         if (reference is ClassDescriptor) usedClasses.add(reference)
-        
+
         expression.acceptChildren(this)
     }
-    
+
+}
+
+class UnusedHintFix(private val parserResult: KotlinParserResult,
+                    private val importDirective: KtImportDirective) : HintFix {
+    override fun isSafe() = true
+
+    override fun isInteractive() = false
+
+    override fun getDescription() = "Remove unused import"
+
+    override fun implement() {
+        val doc = parserResult.snapshot.source.getDocument(false)
+        val startOffset = importDirective.textRange.startOffset - 1
+        val length = importDirective.textRange.endOffset - startOffset
+        doc.remove(startOffset, length)
+    }
 }

99788a83a764de7b1de12e522bfd2bdc8e9e6f38
Dusan Balek <dbalek@netbeans.org>
2017-05-11 14:00:18 +0200

    Issue #268069 - [82cat] Auto complete suggestions don't show first time - fixed.

--- java.source.base/src/org/netbeans/modules/java/source/PostFlowAnalysis.java
+++ java.source.base/src/org/netbeans/modules/java/source/PostFlowAnalysis.java
@@ -166,12 +166,16 @@ public class PostFlowAnalysis extends TreeScanner {
         }
         Type type = types.erasure(tree.type);
         for (Symbol sym : s.getSymbolsByName(tree.name)) {
-            if (sym != tree.sym && !sym.type.isErroneous() && !type.isErroneous() &&
-                !isUnknown(sym.type) && !isUnknown(type) &&
-                types.isSameType(types.erasure(sym.type), type)) {
-                log.error(tree.pos(), "name.clash.same.erasure", tree.sym, sym); //NOI18N
-                return;
-            }
+            try {
+                boolean clash = sym != tree.sym
+                        && !sym.type.isErroneous()
+                        && !type.isErroneous()
+                        && types.isSameType(types.erasure(sym.type), type);
+                if (clash) {
+                    log.error(tree.pos(), "name.clash.same.erasure", tree.sym, sym); //NOI18N
+                    return;
+                }
+            } catch (AssertionError e) {}
         }
     }
 
@@ -250,19 +254,5 @@ public class PostFlowAnalysis extends TreeScanner {
     private void checkStringConstant(DiagnosticPosition pos, Object constValue) {
         if (constValue instanceof String && ((String)constValue).length() >= Pool.MAX_STRING_LENGTH)
             log.error(pos, "limit.string"); //NOI18N
-    }
-    
-    private boolean isUnknown(Type t) {
-        return t != null && t.accept(new Types.DefaultTypeVisitor<Boolean, Void>() {
-            @Override
-            public Boolean visitType(Type t, Void s) {
-                return t.hasTag(TypeTag.UNKNOWN);
-            }
-
-            @Override
-            public Boolean visitMethodType(Type.MethodType t, Void s) {
-                return visit(t.getReturnType(), s);
-            }            
-        }, null);
     }
 }

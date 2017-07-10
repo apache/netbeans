f6ba46234b3538803d7d70994d8d4eb3f8d9c367
Tomas Hurka <thurka@netbeans.org>
2017-07-10 13:37:13 +0200

    release all native screen resources used by window

--- openide.explorer/src/org/openide/explorer/view/CustomPopupFactory.java
+++ openide.explorer/src/org/openide/explorer/view/CustomPopupFactory.java
@@ -197,6 +197,8 @@ class CustomPopupFactory extends PopupFactory {
         
         @Override
         void dispose() {
+            window.setVisible(false);
+            window.dispose();
             window = null;
             super.dispose();
         }

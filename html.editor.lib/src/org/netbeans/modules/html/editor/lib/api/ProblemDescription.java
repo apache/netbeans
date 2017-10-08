2059a010fc2d2813dd6bdb55c2f85f0ec250e0ca
Marty Powell <jpowel25@gmail.com>
2017-10-08 18:12:03 -0400

    roll back because formatting didn't match original

--- html.editor.lib/src/org/netbeans/modules/html/editor/lib/api/ProblemDescription.java
+++ html.editor.lib/src/org/netbeans/modules/html/editor/lib/api/ProblemDescription.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
+ 
 package org.netbeans.modules.html.editor.lib.api;
 
 public final class ProblemDescription {

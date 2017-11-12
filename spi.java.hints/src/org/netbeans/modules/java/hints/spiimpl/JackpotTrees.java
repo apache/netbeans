6c5403a23dad6319719da7c0803387d8721e4476
Jan Lahoda <jlahoda@netbeans.org>
2017-11-12 15:18:17 +0100

    Tree constructors are changing in branches; adding support for creating the tree+ident subclasses dynamically.

--- spi.java.hints/src/org/netbeans/modules/java/hints/spiimpl/JackpotTrees.java
+++ spi.java.hints/src/org/netbeans/modules/java/hints/spiimpl/JackpotTrees.java
@@ -20,6 +20,7 @@
 package org.netbeans.modules.java.hints.spiimpl;
 
 import com.sun.source.tree.IdentifierTree;
+import com.sun.source.tree.Tree.Kind;
 import com.sun.source.tree.TreeVisitor;
 import com.sun.tools.javac.code.Symbol.VarSymbol;
 import com.sun.tools.javac.code.Symtab;
@@ -28,12 +29,29 @@ import com.sun.tools.javac.tree.JCTree.JCAnnotation;
 import com.sun.tools.javac.tree.JCTree.JCBlock;
 import com.sun.tools.javac.tree.JCTree.JCCase;
 import com.sun.tools.javac.tree.JCTree.JCCatch;
+import com.sun.tools.javac.tree.JCTree.JCErroneous;
+import com.sun.tools.javac.tree.JCTree.JCIdent;
 import com.sun.tools.javac.tree.JCTree.JCModifiers;
 import com.sun.tools.javac.tree.JCTree.JCVariableDecl;
+import com.sun.tools.javac.tree.JCTree.Visitor;
 import com.sun.tools.javac.tree.TreeMaker;
 import com.sun.tools.javac.util.Context;
 import com.sun.tools.javac.util.List;
 import com.sun.tools.javac.util.Name;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.description.modifier.Visibility;
+import net.bytebuddy.implementation.FieldAccessor;
+import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.implementation.MethodCall;
+import net.bytebuddy.matcher.ElementMatchers;
 
 
 /**
@@ -41,6 +59,65 @@ import com.sun.tools.javac.util.Name;
  * @author lahvac
  */
 public class JackpotTrees {
+
+    private static final Map<Class<?>, Class<?>> baseClass2Impl = new HashMap<>();
+    public static <T> T createInstance(Context ctx, Class<T> clazz, Name ident, JCIdent jcIdent, Class<?>[] requiredConstructor, Object[] params) {
+        try {
+            Class<?> fake = baseClass2Impl.get(clazz);
+
+            if (fake == null) {
+                Method visitIdent = Visitor.class.getDeclaredMethod("visitIdent", JCIdent.class);
+                Method visitIdentifier = TreeVisitor.class.getDeclaredMethod("visitIdentifier", IdentifierTree.class, Object.class);
+                Method toString = Object.class.getDeclaredMethod("toString");
+                fake = new ByteBuddy()
+                        .subclass(clazz)
+                        .implement(IdentifierTree.class)
+                        .defineField("ident", Name.class, Visibility.PUBLIC)
+                        .defineField("jcIdent", JCIdent.class, Visibility.PUBLIC)
+                        .method(ElementMatchers.named("getName")).intercept(FieldAccessor.ofField("ident"))
+                        .method(ElementMatchers.named("getKind")).intercept(FixedValue.value(Kind.IDENTIFIER))
+                        .method(ElementMatchers.named("accept").and(ElementMatchers.takesArguments(Visitor.class))).intercept(MethodCall.invoke(visitIdent).onArgument(0).withField("jcIdent"))
+                        .method(ElementMatchers.named("accept").and(ElementMatchers.takesArgument(0, TreeVisitor.class))).intercept(MethodCall.invoke(visitIdentifier).onArgument(0).withThis().withArgument(1))
+                        .method(ElementMatchers.named("toString")).intercept(MethodCall.invoke(toString).onField("ident"))
+                        .make()
+                        .load(JackpotTrees.class.getClassLoader())
+                        .getLoaded();
+                baseClass2Impl.put(clazz, fake);
+            }
+
+            NEXT: for (Constructor c : fake.getDeclaredConstructors()) {
+                if (c.getParameterCount() < requiredConstructor.length)
+                    continue;
+                for (int e = 0; e < requiredConstructor.length; e++) {
+                    if (!c.getParameterTypes()[e].equals(requiredConstructor[e])) {
+                        continue NEXT;
+                    }
+                }
+                java.util.List<Object> instances = new ArrayList<>();
+                instances.addAll(Arrays.asList(params));
+                for (int i = instances.size(); i < c.getParameterCount(); i++) {
+                    instances.add(null);
+                }
+
+                JCTree tree = (JCTree) c.newInstance(instances.toArray(new Object[0]));
+
+                Field identField = fake.getDeclaredField("ident");
+
+                identField.set(tree, ident);
+
+                Field jcIdentField = fake.getDeclaredField("jcIdent");
+
+                jcIdentField.set(tree, jcIdent);
+
+                return clazz.cast(tree);
+            }
+
+            throw new IllegalStateException();
+        } catch (IllegalAccessException | IllegalArgumentException | IllegalStateException | InstantiationException | NoSuchFieldException | NoSuchMethodException | SecurityException | InvocationTargetException ex) {
+            throw new IllegalStateException(ex);
+        }
+    }
+
     public static class AnnotationWildcard extends JCAnnotation implements IdentifierTree {
 
         private final Name ident;

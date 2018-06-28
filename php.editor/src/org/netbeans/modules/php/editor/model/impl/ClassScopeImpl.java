junichi11/netbeans-releases

[NETBEANS-949] Assigning docs from another class for autocompletion purposes(mixins)

php.editor/src/org/netbeans/modules/php/editor/model/impl/ClassScopeImpl.java
@@ -89,6 +89,8 @@ import org.openide.util.Union2;
  */
 class ClassScopeImpl extends TypeScopeImpl implements ClassScope, VariableNameFactory {
     private final Collection<QualifiedName> possibleFQSuperClassNames;
+    // @GuardedBy("this")
+    private final Collection<QualifiedName> mixinClassNames = new HashSet<>();
     private final Collection<QualifiedName> usedTraits = new HashSet<>();
     private final Set<? super TypeScope> superRecursionDetection = new HashSet<>();
     private final Set<? super TypeScope> subRecursionDetection = new HashSet<>();
@@ -166,6 +168,7 @@ class ClassScopeImpl extends TypeScopeImpl implements ClassScope, VariableNameFa
         this.superClass = Union2.<String, List<ClassScopeImpl>>createFirst(superClassName != null ? superClassName.toString() : null);
         this.possibleFQSuperClassNames = indexedClass.getPossibleFQSuperClassNames();
         usedTraits.addAll(indexedClass.getUsedTraits());
+        this.mixinClassNames.addAll(indexedClass.getFQMixinClassNames());
     }
     //old contructors
 
@@ -178,6 +181,20 @@ class ClassScopeImpl extends TypeScopeImpl implements ClassScope, VariableNameFa
     @Override
     public Collection<QualifiedName> getPossibleFQSuperClassNames() {
         return this.possibleFQSuperClassNames;
+    }
+
+    /**
+     * Add fully qualified names for @mixin tag.
+     *
+     * @param names fully qualified names
+     */
+    synchronized void addFQMixinClassNames(Collection<QualifiedName> names) {
+        mixinClassNames.addAll(names);
+    }
+
+    @Override
+    public synchronized Collection<QualifiedName> getFQMixinClassNames() {
+        return Collections.unmodifiableCollection(mixinClassNames);
     }
 
     @NonNull
@@ -514,6 +531,16 @@ class ClassScopeImpl extends TypeScopeImpl implements ClassScope, VariableNameFa
         sb.append(Signature.ITEM_DELIMITER);
         sb.append(isDeprecated() ? 1 : 0).append(Signature.ITEM_DELIMITER);
         sb.append(getFilenameUrl()).append(Signature.ITEM_DELIMITER);
+        // mixin
+        StringBuilder mixinSb = new StringBuilder();
+        for (QualifiedName mixinClassName : mixinClassNames) {
+            if (mixinSb.length() > 0) {
+                mixinSb.append(","); // NOI18N
+            }
+            mixinSb.append(mixinClassName.toString());
+        }
+        sb.append(mixinSb);
+        sb.append(Signature.ITEM_DELIMITER);
         return sb.toString();
     }
 
@@ -541,6 +568,8 @@ class ClassScopeImpl extends TypeScopeImpl implements ClassScope, VariableNameFa
         QualifiedName qualifiedName = namespaceScope.getQualifiedName();
         sb.append(qualifiedName.toString()).append(Signature.ITEM_DELIMITER);
         sb.append(isDeprecated() ? 1 : 0).append(Signature.ITEM_DELIMITER);
+        sb.append(getFilenameUrl()).append(Signature.ITEM_DELIMITER);
+        sb.append(Signature.ITEM_DELIMITER); // mixin
 
         return sb.toString();
 

php.editor/src/org/netbeans/modules/php/editor/model/impl/ModelVisitor.java
@@ -1200,6 +1200,24 @@ public final class ModelVisitor extends DefaultTreePathVisitor {
 
     @Override
     public void visit(PHPDocTypeTag node) {
+        // #241740 for @mixin tag
+        if (node.getKind().equals(PHPDocTag.Type.MIXIN)) {
+            Scope currentScope = modelBuilder.getCurrentScope();
+            if (currentScope instanceof ClassScopeImpl) {
+                ClassScopeImpl classScope = (ClassScopeImpl) currentScope;
+                List<? extends PhpDocTypeTagInfo> tagInfos = PhpDocTypeTagInfo.create(node, classScope);
+                Set<QualifiedName> names = new HashSet<>();
+                tagInfos.stream()
+                        .filter(tagInfo -> !tagInfo.getName().isEmpty())
+                        .map(tagInfo -> tagInfo.getTypeName())
+                        .filter(typeName -> (typeName != null && !typeName.isEmpty()))
+                        .map(typeName -> VariousUtils.qualifyTypeNames(typeName, node.getStartOffset(), classScope))
+                        .forEach(qualifiedTypeName -> names.add(QualifiedName.create(qualifiedTypeName)));
+                if (!names.isEmpty()) {
+                    classScope.addFQMixinClassNames(names);
+                }
+            }
+        }
         occurencesBuilder.prepare(node, modelBuilder.getCurrentScope());
         super.visit(node);
     }

apache/netbeans

php.editor/src/org/netbeans/modules/php/editor/model/impl/ClassScopeImpl.java
@@ -45,6 +45,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 import org.netbeans.api.annotations.common.CheckForNull;
@@ -89,6 +90,8 @@ import org.openide.util.Union2;
  */
 class ClassScopeImpl extends TypeScopeImpl implements ClassScope, VariableNameFactory {
     private final Collection<QualifiedName> possibleFQSuperClassNames;
+    // @GuardedBy("this")
+    private final Collection<QualifiedName> mixinClassNames = new LinkedHashSet<>();
     private final Collection<QualifiedName> usedTraits = new HashSet<>();
     private final Set<? super TypeScope> superRecursionDetection = new HashSet<>();
     private final Set<? super TypeScope> subRecursionDetection = new HashSet<>();
@@ -166,6 +169,7 @@ class ClassScopeImpl extends TypeScopeImpl implements ClassScope, VariableNameFa
         this.superClass = Union2.<String, List<ClassScopeImpl>>createFirst(superClassName != null ? superClassName.toString() : null);
         this.possibleFQSuperClassNames = indexedClass.getPossibleFQSuperClassNames();
         usedTraits.addAll(indexedClass.getUsedTraits());
+        this.mixinClassNames.addAll(indexedClass.getFQMixinClassNames());
     }
     //old contructors
 
@@ -178,6 +182,20 @@ class ClassScopeImpl extends TypeScopeImpl implements ClassScope, VariableNameFa
     @Override
     public Collection<QualifiedName> getPossibleFQSuperClassNames() {
         return this.possibleFQSuperClassNames;
+    }
+
+    /**
+     * Add fully qualified names for @mixin tag.
+     *
+     * @param names fully qualified names
+     */
+    synchronized void addFQMixinClassNames(Collection<QualifiedName> names) {
+        mixinClassNames.addAll(names);
+    }
+
+    @Override
+    public synchronized Collection<QualifiedName> getFQMixinClassNames() {
+        return Collections.unmodifiableCollection(mixinClassNames);
     }
 
     @NonNull
@@ -514,6 +532,16 @@ class ClassScopeImpl extends TypeScopeImpl implements ClassScope, VariableNameFa
         sb.append(Signature.ITEM_DELIMITER);
         sb.append(isDeprecated() ? 1 : 0).append(Signature.ITEM_DELIMITER);
         sb.append(getFilenameUrl()).append(Signature.ITEM_DELIMITER);
+        // mixin
+        StringBuilder mixinSb = new StringBuilder();
+        for (QualifiedName mixinClassName : mixinClassNames) {
+            if (mixinSb.length() > 0) {
+                mixinSb.append(","); // NOI18N
+            }
+            mixinSb.append(mixinClassName.toString());
+        }
+        sb.append(mixinSb);
+        sb.append(Signature.ITEM_DELIMITER);
         return sb.toString();
     }
 
@@ -541,6 +569,8 @@ class ClassScopeImpl extends TypeScopeImpl implements ClassScope, VariableNameFa
         QualifiedName qualifiedName = namespaceScope.getQualifiedName();
         sb.append(qualifiedName.toString()).append(Signature.ITEM_DELIMITER);
         sb.append(isDeprecated() ? 1 : 0).append(Signature.ITEM_DELIMITER);
+        sb.append(getFilenameUrl()).append(Signature.ITEM_DELIMITER);
+        sb.append(Signature.ITEM_DELIMITER); // mixin
 
         return sb.toString();
 

php.editor/src/org/netbeans/modules/php/editor/model/impl/ModelVisitor.java
@@ -47,6 +47,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -1200,6 +1201,24 @@ public final class ModelVisitor extends DefaultTreePathVisitor {
 
     @Override
     public void visit(PHPDocTypeTag node) {
+        // #241740 for @mixin tag
+        if (node.getKind().equals(PHPDocTag.Type.MIXIN)) {
+            Scope currentScope = modelBuilder.getCurrentScope();
+            if (currentScope instanceof ClassScopeImpl) {
+                ClassScopeImpl classScope = (ClassScopeImpl) currentScope;
+                List<? extends PhpDocTypeTagInfo> tagInfos = PhpDocTypeTagInfo.create(node, classScope);
+                Set<QualifiedName> names = new LinkedHashSet<>();
+                tagInfos.stream()
+                        .filter(tagInfo -> !tagInfo.getName().isEmpty())
+                        .map(tagInfo -> tagInfo.getTypeName())
+                        .filter(typeName -> (typeName != null && !typeName.isEmpty()))
+                        .map(typeName -> VariousUtils.qualifyTypeNames(typeName, node.getStartOffset(), classScope))
+                        .forEach(qualifiedTypeName -> names.add(QualifiedName.create(qualifiedTypeName)));
+                if (!names.isEmpty()) {
+                    classScope.addFQMixinClassNames(names);
+                }
+            }
+        }
         occurencesBuilder.prepare(node, modelBuilder.getCurrentScope());
         super.visit(node);
     }
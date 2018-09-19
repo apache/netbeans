4266ad1d5df8ddd123419bc4b2eb1c3bed6fb967
Josh Juneau <juneau001@gmail.com>
2018-09-19 06:45:26 -0500

    [NETBEANS-1074] Module review j2ee.kit - Added license headers to files in test/qa-functional/data, as they are not in the rat report
    [NETBEANS-1074] Module review j2ee.kit
    - Modified JSP license header...repaired incorrect comment
    
    [NETBEANS-1074] Module review j2ee.kit
    - Repaired index.jsp license header

--- enterprise/j2ee.kit/test/qa-functional/data/projects/EntityHintsEJB/src/java/hints/A.java
+++ enterprise/j2ee.kit/test/qa-functional/data/projects/EntityHintsEJB/src/java/hints/A.java
@@ -1,4 +1,22 @@
 /*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/*
  * A.java
  *
  * Created on June 19, 2006, 6:36 PM
@@ -25,7 +43,7 @@ public class A implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    
+
     /** Creates a new instance of A */
     public A() {
     }
@@ -57,5 +75,5 @@ public class A implements Serializable {
     public String toString() {
         return "hints.A[id=" + id + "]";
     }
-    
+
 }
--- enterprise/j2ee.kit/test/qa-functional/data/projects/TestCMPRelationships/src/java/test/EntityBBean.java
+++ enterprise/j2ee.kit/test/qa-functional/data/projects/TestCMPRelationships/src/java/test/EntityBBean.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package test;
 
 import javax.ejb.*;
@@ -9,7 +27,7 @@ import javax.ejb.*;
  */
 public abstract class EntityBBean implements EntityBean, EntityBRemoteBusiness, EntityBLocalBusiness {
     private EntityContext context;
-    
+
     // <editor-fold defaultstate="collapsed" desc="EJB infrastructure methods. Click on the + sign on the left to edit the code.">
     // TODO Consider creating Transfer Object to encapsulate data
     // TODO Review finder methods
@@ -19,68 +37,68 @@ public abstract class EntityBBean implements EntityBean, EntityBRemoteBusiness, 
     public void setEntityContext(EntityContext aContext) {
         context = aContext;
     }
-    
+
     /**
      * @see javax.ejb.EntityBean#ejbActivate()
      */
     public void ejbActivate() {
-        
+
     }
-    
+
     /**
      * @see javax.ejb.EntityBean#ejbPassivate()
      */
     public void ejbPassivate() {
-        
+
     }
-    
+
     /**
      * @see javax.ejb.EntityBean#ejbRemove()
      */
     public void ejbRemove() {
-        
+
     }
-    
+
     /**
      * @see javax.ejb.EntityBean#unsetEntityContext()
      */
     public void unsetEntityContext() {
         context = null;
     }
-    
+
     /**
      * @see javax.ejb.EntityBean#ejbLoad()
      */
     public void ejbLoad() {
-        
+
     }
-    
+
     /**
      * @see javax.ejb.EntityBean#ejbStore()
      */
     public void ejbStore() {
-        
+
     }
     // </editor-fold>
-    
-    
+
+
     public abstract String getKey();
     public abstract void setKey(String key);
-    
-    
+
+
     public String ejbCreate(String key)  throws CreateException {
         if (key == null) {
             throw new CreateException("The field \"key\" must not be null");
         }
-        
+
         // TODO add additional validation code, throw CreateException if data is not valid
         setKey(key);
-        
+
         return null;
     }
-    
+
     public void ejbPostCreate(String key) {
         // TODO populate relationships here if appropriate
-        
+
     }
 }

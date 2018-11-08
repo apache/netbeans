d3fbb042ade752e2f9fd02b795483061da20b46b
Reema Taneja <reema.taneja@oracle.com>
2018-11-08 20:50:26 +0530

    [NETBEANS-586] NPE findUsages

--- java/refactoring.java/src/org/netbeans/modules/refactoring/java/plugins/FindUsagesVisitor.java
+++ java/refactoring.java/src/org/netbeans/modules/refactoring/java/plugins/FindUsagesVisitor.java
@@ -393,21 +393,13 @@ public class FindUsagesVisitor extends ErrorAwareTreePathScanner<Tree, Element> 
         Trees trees = workingCopy.getTrees();
         ClassTree classTree = ((NewClassTree) node).getClassBody();
         if (classTree != null && p.getKind() == ElementKind.CONSTRUCTOR) {
-            Element anonClass = workingCopy.getTrees().getElement(TreePath.getPath(workingCopy.getCompilationUnit(), classTree));
-
-            if (anonClass == null) {
-                Logger.getLogger("org.netbeans.modules.refactoring.java").log(Level.SEVERE, "FindUsages cannot resolve {0}", classTree); // NOI18N
-            } else {
-                for (ExecutableElement c : ElementFilter.constructorsIn(anonClass.getEnclosedElements())) {
-                    MethodTree t = workingCopy.getTrees().getTree(c);
-                    if (t == null) {
-                        Logger.getLogger("org.netbeans.modules.refactoring.java").log(Level.SEVERE, "FindUsages cannot resolve {0} {1}", new Object[] {classTree, c}); // NOI18N
-                    } else {
-                        TreePath superCall = trees.getPath(workingCopy.getCompilationUnit(), ((ExpressionStatementTree) t.getBody().getStatements().get(0)).getExpression());
-                        Element superCallElement = trees.getElement(superCall);
-                        if (superCallElement != null && superCallElement.equals(p) && !workingCopy.getTreeUtilities().isSynthetic(superCall)) {
-                            addUsage(superCall);
-                        }
+            for (Tree t : classTree.getMembers()) {
+                Element elem = workingCopy.getTrees().getElement(TreePath.getPath(workingCopy.getCompilationUnit(), t));
+                if (elem.getKind() == ElementKind.CONSTRUCTOR) {
+                    TreePath superCall = trees.getPath(workingCopy.getCompilationUnit(), ((ExpressionStatementTree) ((MethodTree) t).getBody().getStatements().get(0)).getExpression());
+                    Element superCallElement = trees.getElement(superCall);
+                    if (superCallElement != null && superCallElement.equals(p) && !workingCopy.getTreeUtilities().isSynthetic(superCall)) {
+                        addUsage(superCall);
                     }
                 }
             }

3887f56bbb78cc0f6d48a13de4a9e54b80953b6b
Mark Phipps <mwgphipps@gmail.com>
2019-02-13 12:36:01 +0000

    Added some more javadoc.

--- platform/openide.windows/src/org/openide/windows/ModeXml.java
+++ platform/openide.windows/src/org/openide/windows/ModeXml.java
@@ -19,7 +19,19 @@
 package org.openide.windows;
 
 /**
- * Utility class to help clients get the XML configuration of a Mode.
+ * Utility class to help clients manage {@link Mode}s in order to layout TopComponents
+ * in predefined "work-spaces".
+ * <br>
+ * <br>
+ * In a NetBeans Platform application, a user may create various TopComponents and
+ * drag them around, into and out of the existing defined Modes (e.g. editor, explorer) or 
+ * new "anonymous" Modes which get created by the system as needed.
+ * <br>
+ * <br>
+ * The configuration of each Mode can be expressed in XML. This class gives access
+ * to that XML with a mechanism to reproduce a Mode from XML so you can construct 
+ * ways to save Mode and TopComponent combinations, which can subsequently be
+ * reloaded at a later time.
  * 
  * @see http://wiki.apidesign.org/wiki/ExtendingInterfaces
  * 
@@ -42,18 +54,43 @@ public final class ModeXml {
                 : null;
     }
     
+    /**
+     * Given some XML, attempts to ask the WindowManager to create a Mode that can
+     * subsequently be used to dock a TopComponent into. Usually this will be an 
+     * anonymous Mode.
+     * 
+     * @param windowManager typically WindowManager.getDefault()
+     * @param xml the XML that was originally produced by {@link #toXml}
+     */
     public final static void createModeFromXml(WindowManager windowManager, String xml) {
         if(windowManager instanceof WindowManager.ModeManager) {
             ((WindowManager.ModeManager)windowManager).createModeFromXml(xml);
         }
     }
     
+    /**
+     * Before restoring anonymous Modes, it is useful to update whatever defined Modes
+     * may exist like editor, explorer etc., so that all the Modes will eventually
+     * re-appear in the desired locations.
+     * 
+     * @param windowManager typically WindowManager.getDefault()
+     * @param xml the XML that was originally produced by {@link #toXml}
+     */
     public final static void updateModeConstraintsFromXml(WindowManager windowManager, String xml) {
         if(windowManager instanceof WindowManager.ModeManager) {
             ((WindowManager.ModeManager)windowManager).updateModeConstraintsFromXml(xml);
         }
     }
     
+    /**
+     * Before restoring a whole bunch of Modes (for example with XML that has been
+     * previously saved somewhere and now loaded), it is useful to remove the
+     * anonymous modes from the system.
+     * 
+     * @param windowManager typically WindowManager.getDefault()
+     * @param mode the {@link Mode} to remove
+     * @return success if the Mode can no longer be found in the Window Manager
+     */
     public final static boolean removeMode(WindowManager windowManager, Mode mode) {
         return windowManager instanceof WindowManager.ModeManager
                 ? ((WindowManager.ModeManager)windowManager).removeMode(mode)

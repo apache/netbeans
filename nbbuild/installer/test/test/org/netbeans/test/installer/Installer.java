7078efda93f2cd7bcdbfba6f6ffeb57eb14ddb75
Reema Taneja <32299405+rtaneja1@users.noreply.github.com>
2019-05-02 14:42:32 +0530

    [NETBEANS-1671] Donation 3 - Include installer sources (#1052)
    * [NETBEANS-1671] Include donated Installer source in Apache NetBeans
    
    * NB10-JDK11.0.1 bundle related changes
    
    * convert to Apache licence
    
    * convert to Apache licence
    
    * convert to Apache licence
    
    * convert to Apache licence
    
    * update ignored-binaries
    
    * Added support to create NetBeans installers
    
    * updated bg image in mac installer
    
    * moved installer from root to under nbbuild
    
    * moved installer to nbbuild
    
    * Update ignored-binaries
    
    * license updates and removed external binaries
    
    * updated ignore-binaries
    
    * added missing license text
    
    * adding git ignored files under infra/build
    
    * misc build related changes
    
    * fix build issues
    
    * fix build issues
    
    * fix file name typo
    
    * bin name change
    
    * mac installer build related changes
    
    * more fixes
    
    * minor fixes
    
    * mac installer signing support
    
    * Removed files not used in installer - set 1
    
    * removed unused installer code
    
    * Revert "removed unused installer code"
    
    This reverts commit 7b078833890f223271ee8b172a2ae669a6576b66.
    
    * Create binaries
    
    * Delete binaries
    
    * Fix JDK 12 install not showing in jdkhomes dropdown
    
    * removed code not currently being used in installers
    
    * removed files not used in installer
    
    * undo icon file change to avoid conflic with pr 1196

---
+++ nbbuild/installer/test/test/org/netbeans/test/installer/Installer.java
@@ -0,0 +1,86 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.netbeans.test.installer;
+
+import java.util.logging.Logger;
+
+/**
+ *
+
+ */
+public class Installer {
+
+    @org.junit.Test
+    public void testInstaller() {
+        TestData data = new TestData(Logger.getLogger("global"));
+
+        Utils.phaseOne(data, "all");
+
+        // Pages
+          // Apache
+        Utils.stepChooseComponet( "Apache Tomcat", data );
+        // Welcome
+        Utils.stepWelcome();
+        // Agreement
+        Utils.stepLicense();
+        // Location
+        Utils.stepSetDir(
+            data,
+            "Install the NetBeans IDE",
+            data.GetNetBeansInstallPath( )
+          );
+        // GF
+        Utils.stepSetDir(
+            data,
+            "Install GlassFish",
+            data.GetApplicationServerInstallPath( )
+          );
+        if( data.m_bPreludePresents )
+        {
+          Utils.stepSetDir(
+              data,
+              "Install GlassFish prelude",
+              data.GetApplicationServerPreludeInstallPath( )
+            );
+        }
+        // Apache
+        Utils.stepSetDir(
+            data,
+            "Install Apache Tomcat",
+            data.GetTomcatInstallPath( )
+          );
+        // Summary
+        Utils.stepInstall(data);
+        //Installation
+        //finish
+        Utils.stepFinish();
+
+        //Utils.phaseTwo(data);
+        //Utils.phaseThree(data);
+        Utils.phaseFour(data);
+
+        //Utils.RunCommitTests( data );
+
+        Utils.phaseFive( data );
+
+        //Utils.phaseFourWOUninstall(data);
+      //TODO Dir removed test
+      //TODO Clean up work dir
+    }
+}

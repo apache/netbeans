b128680bcea43fbef358d8d808f17e506c48fb49
gbrinkmann <g.brinkmann@mce-bank.eu>
2019-05-13 12:01:55 +0200

    [NETBEANS-1936] Version splitting Testcode
    test method testSplitting added

--- ide/subversion/test/unit/src/org/netbeans/modules/subversion/client/cli/commands/VersionCommandTest.java
+++ ide/subversion/test/unit/src/org/netbeans/modules/subversion/client/cli/commands/VersionCommandTest.java
@@ -30,6 +30,20 @@ public class VersionCommandTest {
         assertNotNull(Version.parse("1.2"));
         assertNotNull(Version.parse("1.10.0 (r1827917)"));
     }
+    
+    @Test
+    public void testSplitting() {
+        assertFalse(Version.parse(" version 1.2.3").lowerThan(Version.parse("1.2.3")));
+        assertFalse(Version.parse(" version 1.2.3").greaterThan(Version.parse("1.2.3")));
+        assertFalse(Version.parse(" version 0-dev").lowerThan(Version.parse("0")));
+        assertFalse(Version.parse(" version 0-dev").greaterThan(Version.parse("0")));
+        assertFalse(Version.parse(" version 1.2.3-dev").lowerThan(Version.parse("1.2.3")));
+        assertFalse(Version.parse(" version 1.2.3-dev").greaterThan(Version.parse("1.2.3")));
+        assertFalse(Version.parse(" version 1.2.3-SNAPSHOT").lowerThan(Version.parse("1.2.3")));
+        assertFalse(Version.parse(" version 1.2.3-SNAPSHOT").greaterThan(Version.parse("1.2.3")));
+        assertFalse(Version.parse("svn, version 1.9.7-SlikSvn (SlikSvn/1.9.7)").lowerThan(Version.parse("1.9.7")));
+        assertFalse(Version.parse("svn, version 1.9.7-SlikSvn (SlikSvn/1.9.7)").greaterThan(Version.parse("1.9.7")));
+    }
 
     @Test
     public void testComparison() {

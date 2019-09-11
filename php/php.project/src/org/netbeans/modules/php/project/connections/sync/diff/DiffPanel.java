08549112915b2b09c666ad23904cb14bf8d80f82
Junichi Yamamoto <junichi11@apache.org>
2019-09-11 20:35:57 +0900

    Keep the last diff window size

--- php/php.project/src/org/netbeans/modules/php/project/connections/sync/diff/DiffPanel.java
+++ php/php.project/src/org/netbeans/modules/php/project/connections/sync/diff/DiffPanel.java
@@ -62,6 +62,8 @@ public final class DiffPanel extends JPanel {
     static final Logger LOGGER = Logger.getLogger(DiffPanel.class.getName());
 
     private static final RequestProcessor RP = new RequestProcessor(DiffPanel.class);
+    // @GuardedBy(AWT)
+    private static Dimension PREFERRED_SIZE = new Dimension(1024, 768);
 
     final SyncItem syncItem;
     final RemoteClient remoteClient;
@@ -84,7 +86,7 @@ public final class DiffPanel extends JPanel {
         this.charsetName = charsetName;
 
         initComponents();
-        setPreferredSize(new Dimension(1024, 768));
+        setPreferredSize(PREFERRED_SIZE);
     }
 
     @NbBundle.Messages({
@@ -112,6 +114,7 @@ public final class DiffPanel extends JPanel {
         try {
             dialog.setVisible(true);
         } finally {
+            PREFERRED_SIZE = getSize();
             dialog.dispose();
             DiffFileEncodingQueryImpl.clear();
         }

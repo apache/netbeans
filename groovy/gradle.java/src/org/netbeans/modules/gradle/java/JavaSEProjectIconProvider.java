e82444fd1f8665af6badd7959e73964b463345ba
Laszlo Kishalmi <laszlo.kishalmi@gmail.com>
2019-10-07 19:03:09 -0700

    [NETBEANS-2417] Cleaned up the Gradle icon handling

--- groovy/gradle.java/src/org/netbeans/modules/gradle/java/JavaSEProjectIconProvider.java
+++ groovy/gradle.java/src/org/netbeans/modules/gradle/java/JavaSEProjectIconProvider.java
@@ -16,52 +16,45 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+package org.netbeans.modules.gradle.java;
 
-package org.netbeans.modules.gradle.queries;
-
-import org.netbeans.modules.gradle.api.GradleBaseProject;
-import org.netbeans.modules.gradle.api.NbGradleProject;
-import org.netbeans.modules.gradle.spi.ProjectIconProvider;
 import java.awt.Image;
 import java.util.Set;
 import org.netbeans.api.annotations.common.StaticResource;
 import org.netbeans.api.project.Project;
+import org.netbeans.modules.gradle.api.GradleBaseProject;
+import org.netbeans.modules.gradle.api.NbGradleProject;
+import org.netbeans.modules.gradle.spi.ProjectIconProvider;
+import org.netbeans.spi.project.LookupProvider.Registration.ProjectType;
 import org.netbeans.spi.project.ProjectServiceProvider;
 import org.openide.util.ImageUtilities;
 
 /**
  *
- * @author Laszlo Kishalmi
+ * @author lkishalmi
  */
-@ProjectServiceProvider(service = ProjectIconProvider.class, projectType = NbGradleProject.GRADLE_PROJECT_TYPE)
-public class DefaultProjectIconProvider implements ProjectIconProvider {
+@ProjectServiceProvider(service = ProjectIconProvider.class, projectTypes = {
+    @ProjectType(id = NbGradleProject.GRADLE_PROJECT_TYPE, position = 1000)
+})
+public class JavaSEProjectIconProvider implements ProjectIconProvider {
 
     @StaticResource
-    private static final String GRADLE_ICON = "org/netbeans/modules/gradle/resources/gradle.png"; //NOI18
-
-    @StaticResource
-    private static final String GRADLE_JAVASE_ICON = "org/netbeans/modules/gradle/resources/javaseProjectIcon.png"; //NOI18
-    
-    @StaticResource
-    private static final String APPLICATION_BADGE = "org/netbeans/modules/gradle/resources/application-badge.png"; //NOI18
+    private static final String GRADLE_JAVASE_ICON = "org/netbeans/modules/gradle/java/resources/javaseProjectIcon.png"; //NOI18
 
     final Project project;
 
-    public DefaultProjectIconProvider(Project project) {
+    public JavaSEProjectIconProvider(Project project) {
         this.project = project;
     }
-    
+
     @Override
     public Image getIcon() {
+        Image ret = null;
         Set<String> plugins = GradleBaseProject.get(project).getPlugins();
-        String iconResource = plugins.contains("java") ? GRADLE_JAVASE_ICON: GRADLE_ICON;
-        Image ret = ImageUtilities.loadImage(iconResource);
-        
-        if (plugins.contains("application")) {
-            Image badge = ImageUtilities.loadImage(APPLICATION_BADGE);
-            ret = ImageUtilities.mergeImages(ret, badge, 8, 8);
+        if (plugins.contains("java")) { //NOI18N
+            ret = ImageUtilities.loadImage(GRADLE_JAVASE_ICON);
         }
-        return ret;
+        return  ret;
     }
 
     @Override
@@ -69,5 +62,4 @@ public class DefaultProjectIconProvider implements ProjectIconProvider {
         return false;
     }
 
-    
 }

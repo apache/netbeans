5c6b231bf4ba02d9ddbc5ba6ab0d54a17f576774
Laszlo Kishalmi <laszlo.kishalmi@gmail.com>
2019-11-23 11:45:04 -0800

    [NETBEANS-3432] Removed the auto activation code from Dark LaF Support

--- platform/o.n.swing.laf.dark/src/org/netbeans/swing/laf/dark/Installer.java
+++ platform/o.n.swing.laf.dark/src/org/netbeans/swing/laf/dark/Installer.java
@@ -19,88 +19,15 @@
 
 package org.netbeans.swing.laf.dark;
 
-import java.lang.reflect.Method;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.prefs.Preferences;
 import javax.swing.UIManager;
 import org.openide.modules.ModuleInstall;
-import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
-import org.openide.util.NbPreferences;
-import org.openide.windows.WindowManager;
 
-/**
- * 
- */
 public class Installer extends ModuleInstall {
-
-    private static boolean switchEditorColors = false;
-
-    @Override
-    public void restored() {
-        if( switchEditorColors ) {
-            WindowManager.getDefault().invokeWhenUIReady( new Runnable() {
-
-                @Override
-                public void run() {
-                    switchEditorColorsProfile();
-                }
-            });
-        };
-    }
 
     @Override
     public void validate() throws IllegalStateException {
-        Preferences prefs = getPreferences();
-        if( !prefs.getBoolean("dark.themes.installed", false) ) { //NOI18N
-            prefs.put( "laf", DarkMetalLookAndFeel.class.getName() ); //NOI18N
-            switchEditorColors = true;
-        }
-        prefs.putBoolean( "dark.themes.installed", true ); //NOI18N
         UIManager.installLookAndFeel(new UIManager.LookAndFeelInfo( NbBundle.getMessage(Installer.class, "LBL_DARK_METAL"), DarkMetalLookAndFeel.class.getName()) );
         UIManager.installLookAndFeel(new UIManager.LookAndFeelInfo( NbBundle.getMessage(Installer.class, "LBL_DARK_NIMBUS"), DarkNimbusLookAndFeel.class.getName()) );
-    }
-
-    //Use reflection to instantiate ColorModel class and get/set the current profile
-    private static final String COLOR_MODEL_CLASS_NAME = "org.netbeans.modules.options.colors.ColorModel"; //NOI18N
-    private static final String DARK_COLOR_THEME_NAME = "Norway Today"; //NOI18N
-
-    private boolean isChangeEditorColorsPossible() {
-        ClassLoader cl = Lookup.getDefault().lookup( ClassLoader.class );
-        if( null == cl )
-            cl = Installer.class.getClassLoader();
-        try {
-            Class klz = cl.loadClass( COLOR_MODEL_CLASS_NAME );
-            Object colorModel = klz.newInstance();
-            Method m = klz.getDeclaredMethod( "getCurrentProfile", new Class[0] ); //NOI18N
-            Object res = m.invoke( colorModel, new Object[0] );
-            return res != null && !DARK_COLOR_THEME_NAME.equals(res);
-        } catch( Exception ex ) {
-            //ignore
-        }
-        return false;
-    }
-
-    private void switchEditorColorsProfile() {
-        if( !isChangeEditorColorsPossible() )
-            return;
-
-        ClassLoader cl = Lookup.getDefault().lookup( ClassLoader.class );
-        if( null == cl )
-            cl = Installer.class.getClassLoader();
-        try {
-            Class klz = cl.loadClass( COLOR_MODEL_CLASS_NAME );
-            Object colorModel = klz.newInstance();
-            Method m = klz.getDeclaredMethod( "setCurrentProfile", String.class ); //NOI18N
-            m.invoke( colorModel, DARK_COLOR_THEME_NAME );
-        } catch( Exception ex ) {
-            //ignore
-            Logger.getLogger( Installer.class.getName() ).log( Level.INFO, "Cannot change editor colors profile.", ex ); //NOI18N
-        }
-    }
-
-    private Preferences getPreferences() {
-        return NbPreferences.root().node( "laf" ); //NOI18N
     }
 }

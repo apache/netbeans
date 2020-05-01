dba9e54e90cb2cf13fa3305532469fcb899e1b4a
Hector Espert <hectorespertpardo@gmail.com>
2020-05-01 10:52:49 +0200

    Remove unused import

--- ergonomics/ide.ergonomics/src/org/netbeans/modules/ide/ergonomics/fod/ConfigurationPanel.java
+++ ergonomics/ide.ergonomics/src/org/netbeans/modules/ide/ergonomics/fod/ConfigurationPanel.java
@@ -30,7 +30,6 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import static java.util.Objects.nonNull;
 import java.util.Set;
 import java.util.concurrent.Callable;

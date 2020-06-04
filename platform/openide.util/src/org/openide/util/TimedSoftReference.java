4099e687c7688cf4308c25fa250f77004af7da49
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2020-06-04 18:15:58 +0200

    Allow usage of NbBundle in thread-less environments

--- platform/openide.util/src/org/openide/util/TimedSoftReference.java
+++ platform/openide.util/src/org/openide/util/TimedSoftReference.java
@@ -67,8 +67,14 @@ final class TimedSoftReference<T> extends SoftReference<T> implements Runnable {
         this.o = o;
         this.m = m;
         this.k = k;
-        task = RP.create(this);
-        task.schedule(TIMEOUT);
+        try {
+            this.task = RP.create(this);
+            this.task.schedule(TIMEOUT);
+        } catch (SecurityException ex) {
+            // behave as regular SoftReference
+            this.o = null;
+            this.task = null;
+        }
     }
 
     public void run() {
@@ -102,7 +108,9 @@ final class TimedSoftReference<T> extends SoftReference<T> implements Runnable {
                 // touch me
                 //System.err.println("Touch " + k);
                 if (touched == 0) {
-                    task.schedule(TIMEOUT);
+                    if (task != null) {
+                        task.schedule(TIMEOUT);
+                    }
                 }
 
                 touched = System.currentTimeMillis();

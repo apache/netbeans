83ec2c782e1f4f148516837bf184e7d61b6aac33
Hector Espert <hectorespertpardo@gmail.com>
2020-06-05 22:50:33 +0200

    Update Jenkinsfile.groovy

--- nbbuild/jenkins/commit/Jenkinsfile.groovy
+++ nbbuild/jenkins/commit/Jenkinsfile.groovy
@@ -27,9 +27,19 @@ pipeline {
     
     stages {
         
-        stage("Clean") {
+        stage("PR Commit validation") {
+
             steps {
               echo "Test"
+                try {
+            echo 'Hello World'
+        } catch (err) {
+            // CHANGE_ID is set only for pull requests, so it is safe to access the pullRequest global variable
+            if (env.CHANGE_ID) {
+                pullRequest.addLabel('Build Failed')
+            }
+            throw err
+        }
             }
         }
         

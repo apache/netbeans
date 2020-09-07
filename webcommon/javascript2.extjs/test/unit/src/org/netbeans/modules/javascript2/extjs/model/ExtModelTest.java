843e942b18498efb44c00e0be5ea786ace7d6ddb
Matthias Bläsing <mblaesing@doppel-helix.eu>
2020-09-07 20:22:02 +0200

    Fix unittest for webcommon caused by download failures from cdnjs
    It seems that for some networks CDNJS requires sending a User-Agent
    header. This changeset adds it to the requests.

--- webcommon/javascript2.extjs/test/unit/src/org/netbeans/modules/javascript2/extjs/model/ExtModelTest.java
+++ webcommon/javascript2.extjs/test/unit/src/org/netbeans/modules/javascript2/extjs/model/ExtModelTest.java
@@ -23,6 +23,7 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
+import java.net.URLConnection;
 import org.netbeans.modules.javascript2.model.ModelTestBase;
 
 /**
@@ -36,11 +37,14 @@ public class ExtModelTest extends ModelTestBase {
     }
 
     @Override
+    @SuppressWarnings("NestedAssignment")
     protected void setUp() throws Exception {
         super.setUp();
         File classManagerJs = new File(getDataDir(), "testfiles/completion/applyMethod/ClassManager.js");
         if(! classManagerJs.exists()) {
             URL source = new URL("https://cdnjs.cloudflare.com/ajax/libs/extjs/4.2.1/src/class/ClassManager.js");
+            URLConnection connection = source.openConnection();
+            connection.addRequestProperty("User-Agent", "NetBeans Unittesting");
             try(InputStream is = source.openStream();
                 OutputStream os = new FileOutputStream(classManagerJs)) {
                 byte[] buffer = new byte[1024 * 10];
--- webcommon/javascript2.extjs/test/unit/src/org/netbeans/modules/javascript2/extjs/model/JsStructureTest.java
+++ webcommon/javascript2.extjs/test/unit/src/org/netbeans/modules/javascript2/extjs/model/JsStructureTest.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
+import java.net.URLConnection;
 import org.netbeans.modules.javascript2.editor.JsTestBase;
 import org.openide.filesystems.FileObject;
 
@@ -38,11 +39,14 @@ public class JsStructureTest extends JsTestBase {
     }
 
     @Override
+    @SuppressWarnings("NestedAssignment")
     protected void setUp() throws Exception {
         super.setUp();
         File classManagerJs = new File(getDataDir(), "testfiles/completion/applyMethod/ClassManager.js");
         if(! classManagerJs.exists()) {
             URL source = new URL("https://cdnjs.cloudflare.com/ajax/libs/extjs/4.2.1/src/class/ClassManager.js");
+            URLConnection connection = source.openConnection();
+            connection.addRequestProperty("User-Agent", "NetBeans Unittesting");
             try(InputStream is = source.openStream();
                 OutputStream os = new FileOutputStream(classManagerJs)) {
                 byte[] buffer = new byte[1024 * 10];

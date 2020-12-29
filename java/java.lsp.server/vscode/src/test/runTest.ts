cf0d617303c2f3c1fabeb1bb14d3eba9bec1087b
Jan Lahoda <jlahoda@netbeans.org>
2020-12-29 13:50:36 +0100

    Attempting to fix tests.

--- java/java.lsp.server/vscode/src/test/runTest.ts
+++ java/java.lsp.server/vscode/src/test/runTest.ts
@@ -23,7 +23,7 @@ async function main() {
 
         // Download VS Code, unzip it and run the integration test
         await runTests({
-            version: "1.50.0",
+            version: "1.52.0",
             extensionDevelopmentPath,
             extensionTestsPath,
             extensionTestsEnv: {

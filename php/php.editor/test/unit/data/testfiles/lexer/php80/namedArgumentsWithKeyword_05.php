77d56bff2648eac2687a24e42dbe26259efa5500
Junichi Yamamoto <junichi11@apache.org>
2021-01-24 14:27:47 +0900

    [NETBEANS-4443] PHP 8.0 Support: Named Arguments (Part 3)
    https://issues.apache.org/jira/browse/NETBEANS-4443
    
    - https://wiki.php.net/rfc/named_params
    - Don't highlight parameter names when they are keywords
    
    e.g.
    ```php
    test(array: [1, 2], default: "default");
    ```
    
    Notes:
    
    - "default:" is used in a switch statement
    - "true:", "false:", and "null:" can be used with a ternary operator
    - "else:" is used in alternative syntax for a control structure

---
+++ php/php.editor/test/unit/data/testfiles/lexer/php80/namedArgumentsWithKeyword_05.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#[A(static: "test", default: self::CONSTANT)]
+class NamedArgumentsWithKeyword
+{
+    public const CONSTANT = "CONSTANT";
+
+    public function __construct($array, $default) {
+    }
+
+    public function test() {
+        parent::staticReservedKeyword(array: [1, 2], default: "default");
+        self::staticReservedKeyword(array: [1, 2], default: "default");
+        static::staticReservedKeyword(array: [1, 2], default: "default");
+        $this->reservedKeyword(string: "test", int: 1);
+
+        // default keyword in switch statement
+        switch ($test) {
+            case self::CONSTANT:
+                break;
+            default: // default keyword
+                break;
+        }
+
+        $result = match ($a) {
+            1 => test(),
+            default => 0,
+        };
+    }
+
+    public function reservedKeyword() {}
+    public static function staticReservedKeyword() {}
+}
+
+reservedKeyword(array: [1, 2], default: "default");
+reservedKeyword(array: [1, reservedKeyword(default: "default")], default: "default");
+reservedKeyword(default: reservedKeyWord(array: [0, 2]));
+
+// default keyword in switch statement
+switch ($test) {
+    case $value:
+        break;
+    default: // default keyword
+        break;
+}
+
+$result = match ($a) {
+    1 => test(),
+    default => 0,
+};

5ac374ed20e6386835319742f1aa18a499720316
Jan Lahoda <jlahoda@netbeans.org>
2021-03-28 10:14:50 +0200

    Adding annotations to test method, so that they can be run more easily from the glyph gutter.

---
+++ ide/gsf.testrunner.ui/src/org/netbeans/modules/gsf/testrunner/ui/annotation/TestMethodAnnotation.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.netbeans.modules.gsf.testrunner.ui.annotation;
+
+import org.netbeans.modules.gsf.testrunner.ui.api.TestMethodController;
+import org.openide.text.Annotation;
+
+/**
+ *
+ * @author lahvac
+ */
+public class TestMethodAnnotation extends Annotation {
+    
+    public static final Object DOCUMENT_METHODS_KEY = new Object() {};
+    public static final Object DOCUMENT_ANNOTATIONS_KEY = new Object() {};
+    public static final Object DOCUMENT_ANNOTATION_LINES_KEY = new Object() {};
+
+    private final TestMethodController.TestMethod testMethod;
+
+    public TestMethodAnnotation(TestMethodController.TestMethod testMethod) {
+        this.testMethod = testMethod;
+    }
+
+    @Override
+    public String getAnnotationType() {
+        return "org-netbeans-modules-gsf-testrunner-runnable-test-annotation";
+    }
+
+    @Override
+    public String getShortDescription() {
+        return "XXX: test method";
+    }
+    
+}

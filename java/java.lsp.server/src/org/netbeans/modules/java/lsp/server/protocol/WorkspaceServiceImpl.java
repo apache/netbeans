6cd1a36fcdbed041817b17080fb5c78819b807d4
Dusan Balek <dusan.balek@oracle.com>
2021-05-25 13:02:41 +0200

    LSP: Get project source roots, classpath, and packages commands added. (#2971)

--- java/java.lsp.server/src/org/netbeans/modules/java/lsp/server/protocol/WorkspaceServiceImpl.java
+++ java/java.lsp.server/src/org/netbeans/modules/java/lsp/server/protocol/WorkspaceServiceImpl.java
@@ -30,6 +30,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.EnumSet;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -59,7 +60,9 @@ import org.netbeans.api.annotations.common.NullAllowed;
 import org.netbeans.api.debugger.ActionsManager;
 import org.netbeans.api.debugger.DebuggerManager;
 import org.netbeans.api.java.project.JavaProjectConstants;
+import org.netbeans.api.java.queries.SourceForBinaryQuery;
 import org.netbeans.api.java.queries.UnitTestForSourceQuery;
+import org.netbeans.api.java.source.ClassIndex;
 import org.netbeans.api.java.source.ClasspathInfo;
 import org.netbeans.api.java.source.CompilationController;
 import org.netbeans.api.java.source.CompilationInfo;
@@ -136,6 +139,48 @@ public final class WorkspaceServiceImpl implements WorkspaceService, LanguageCli
                 progressOfCompilation.checkStatus();
                 return progressOfCompilation.getFinishFuture();
             }
+            case Server.JAVA_GET_PROJECT_SOURCE_ROOTS: {
+                String uri = ((JsonPrimitive) params.getArguments().get(0)).getAsString();
+                String type = params.getArguments().size() > 1 ? ((JsonPrimitive) params.getArguments().get(1)).getAsString() : JavaProjectConstants.SOURCES_TYPE_JAVA;
+                return getSourceRoots(uri, type).thenApply(roots -> {
+                    return roots.stream().map(root -> Utils.toUri(root)).collect(Collectors.toList());
+                });
+            }
+            case Server.JAVA_GET_PROJECT_CLASSPATH: {
+                String uri = ((JsonPrimitive) params.getArguments().get(0)).getAsString();
+                ClasspathInfo.PathKind kind = params.getArguments().size() > 1 ? ClasspathInfo.PathKind.valueOf(((JsonPrimitive) params.getArguments().get(1)).getAsString()) : ClasspathInfo.PathKind.COMPILE;
+                boolean preferSources = params.getArguments().size() > 2 ? ((JsonPrimitive) params.getArguments().get(2)).getAsBoolean() : false;
+                return getSourceRoots(uri, JavaProjectConstants.SOURCES_TYPE_JAVA).thenApply(roots -> {
+                    HashSet<FileObject> cpRoots = new HashSet<>();
+                    for(FileObject root : roots) {
+                        for (FileObject cpRoot : ClasspathInfo.create(root).getClassPath(kind).getRoots()) {
+                            FileObject[] srcRoots = preferSources ? SourceForBinaryQuery.findSourceRoots(cpRoot.toURL()).getRoots() : null;
+                            if (srcRoots != null && srcRoots.length > 0) {
+                                for (FileObject srcRoot : srcRoots) {
+                                    cpRoots.add(srcRoot);
+                                }
+                            } else {
+                                cpRoots.add(cpRoot);
+                            }
+                        }
+                    }
+                    return cpRoots.stream().map(fo -> Utils.toUri(fo)).collect(Collectors.toList());
+                });
+            }
+            case Server.JAVA_GET_PROJECT_PACKAGES: {
+                String uri = ((JsonPrimitive) params.getArguments().get(0)).getAsString();
+                boolean srcOnly = params.getArguments().size() > 1 ? ((JsonPrimitive) params.getArguments().get(1)).getAsBoolean() : false;
+                return getSourceRoots(uri, JavaProjectConstants.SOURCES_TYPE_JAVA).thenApply(roots -> {
+                    HashSet<String> packages = new HashSet<>();
+                    EnumSet<ClassIndex.SearchScope> scope = srcOnly ? EnumSet.of(ClassIndex.SearchScope.SOURCE) : EnumSet.allOf(ClassIndex.SearchScope.class);
+                    for(FileObject root : roots) {
+                        packages.addAll(ClasspathInfo.create(root).getClassIndex().getPackageNames("", false, scope));
+                    }
+                    ArrayList<String> ret = new ArrayList<>(packages);
+                    Collections.sort(ret);
+                    return ret;
+                });
+            }
             case Server.JAVA_LOAD_WORKSPACE_TESTS: {
                 String uri = ((JsonPrimitive) params.getArguments().get(0)).getAsString();
                 FileObject file;
@@ -208,6 +253,29 @@ public final class WorkspaceServiceImpl implements WorkspaceService, LanguageCli
                 configDispNames.add(c.getDisplayName());
             }
             return configDispNames;
+        });
+    }
+
+    private CompletableFuture<List<FileObject>> getSourceRoots(String uri, String type) {
+        FileObject file;
+        try {
+            file = URLMapper.findFileObject(new URL(uri));
+        } catch (MalformedURLException ex) {
+            Exceptions.printStackTrace(ex);
+            return CompletableFuture.completedFuture(Collections.emptyList());
+        }
+        if (file == null) {
+            return CompletableFuture.completedFuture(Collections.emptyList());
+        }
+        return server.asyncOpenFileOwner(file).thenApply(project -> {
+            if (project != null) {
+                List<FileObject> roots = new ArrayList<>();
+                for(SourceGroup sourceGroup : ProjectUtils.getSources(project).getSourceGroups(type)) {
+                    roots.add(sourceGroup.getRootFolder());
+                }
+                return roots;
+            }
+            return Collections.emptyList();
         });
     }
 

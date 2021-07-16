apache/incubator-netbeans

[NETBEANS-5683] - remove last remaining HashSet raw type warnings..
This change will remove the last remaining warnings about raw type HashSet.

   [repeat] /home/bwalker/src/netbeans/platform/core.multiview/src/org/netbeans/core/multiview/MultiViewActionMap.java:124: warning: [rawtypes] found raw type: HashSet
   [repeat]         Set keys = new HashSet();
   [repeat]                        ^
   [repeat]   missing type arguments for generic class HashSet<E>
   [repeat]   where E is a type-variable:
   [repeat]     E extends Object declared in class HashSet

At this point, we have removed all warnings related to this.. Hooray!!

platform/openide.options/src/org/openide/options/VetoSystemOption.java
@@ -23,6 +23,7 @@ import java.beans.PropertyVetoException;
 import java.beans.VetoableChangeListener;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 
 /** Extends the functionality of <CODE>SystemOption</CODE>
@@ -45,11 +46,11 @@ public abstract class VetoSystemOption extends SystemOption {
     /** Lazy getter for veto hashtable.
     * @return the hashtable
     */
-    private HashSet getVeto() {
-        HashSet set = (HashSet) getProperty(PROP_VETO_SUPPORT);
+    private Set<VetoableChangeListener> getVeto() {
+        Set<VetoableChangeListener> set = (Set<VetoableChangeListener>)getProperty(PROP_VETO_SUPPORT);
 
         if (set == null) {
-            set = new HashSet();
+            set = new HashSet<>();
             putProperty(PROP_VETO_SUPPORT, set);
         }
 
@@ -87,7 +88,7 @@ public abstract class VetoSystemOption extends SystemOption {
         Iterator<VetoableChangeListener> en;
 
         synchronized (getLock()) {
-            en = ((HashSet<VetoableChangeListener>)getVeto().clone()).iterator();
+            en = getVeto().stream().collect(Collectors.toSet()).iterator();
         }
 
         while (en.hasNext()) {

apache/netbeans

platform/openide.options/src/org/openide/options/VetoSystemOption.java
@@ -23,6 +23,7 @@ import java.beans.PropertyVetoException;
 import java.beans.VetoableChangeListener;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 
 /** Extends the functionality of <CODE>SystemOption</CODE>
@@ -45,11 +46,11 @@ public abstract class VetoSystemOption extends SystemOption {
     /** Lazy getter for veto hashtable.
     * @return the hashtable
     */
-    private HashSet getVeto() {
-        HashSet set = (HashSet) getProperty(PROP_VETO_SUPPORT);
+    private Set<VetoableChangeListener> getVeto() {
+        Set<VetoableChangeListener> set = (HashSet<VetoableChangeListener>)getProperty(PROP_VETO_SUPPORT);
 
         if (set == null) {
-            set = new HashSet();
+            set = new HashSet<>();
             putProperty(PROP_VETO_SUPPORT, set);
         }
 
@@ -87,7 +88,7 @@ public abstract class VetoSystemOption extends SystemOption {
         Iterator<VetoableChangeListener> en;
 
         synchronized (getLock()) {
-            en = ((HashSet<VetoableChangeListener>)getVeto().clone()).iterator();
+            en = getVeto().stream().collect(Collectors.toSet()).iterator();
         }
 
         while (en.hasNext()) {
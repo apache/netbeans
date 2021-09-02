6256ee24065729f062c0fa5acbbab80dc4e993f3
Martin Entlicher <martin.entlicher@oracle.com>
2021-09-02 09:28:47 +0200

    Proper handling of LSP logging breakpoints.

--- java/java.lsp.server/src/org/netbeans/modules/java/lsp/server/debugging/breakpoints/NbBreakpoint.java
+++ java/java.lsp.server/src/org/netbeans/modules/java/lsp/server/debugging/breakpoints/NbBreakpoint.java
@@ -33,6 +33,7 @@ import org.eclipse.lsp4j.debug.Source;
 
 import org.netbeans.api.debugger.Breakpoint;
 import org.netbeans.api.debugger.DebuggerManager;
+import org.netbeans.api.debugger.jpda.JPDABreakpoint;
 import org.netbeans.api.debugger.jpda.LineBreakpoint;
 import org.netbeans.modules.debugger.jpda.truffle.breakpoints.TruffleLineBreakpoint;
 import org.netbeans.modules.java.lsp.server.debugging.DebugAdapterContext;
@@ -92,6 +93,8 @@ public final class NbBreakpoint {
             this.hitCount = hitCount;
             if (hitCount > 0) {
                 breakpoint.setHitCountFilter(hitCount, Breakpoint.HIT_COUNT_FILTERING_STYLE.GREATER);
+            } else {
+                breakpoint.setHitCountFilter(0, null);
             }
         }
     }
@@ -112,7 +115,9 @@ public final class NbBreakpoint {
                 b.setCondition(condition);
             }
             if (logMessage != null && !logMessage.isEmpty()) {
-                b.setPrintText(logMessage);
+                String message = lsp2NBLogMessage(logMessage);
+                b.setPrintText(message);
+                b.setSuspend(JPDABreakpoint.SUSPEND_NONE);
             }
             breakpoint = b;
         } else {
@@ -127,6 +132,11 @@ public final class NbBreakpoint {
             if (condition != null && !condition.isEmpty()) {
                 b.setCondition(condition);
             }
+            if (logMessage != null && !logMessage.isEmpty()) {
+                String message = lsp2NBLogMessage(logMessage);
+                b.setPrintText(message);
+                b.setSuspend(false);
+            }
             breakpoint = b;
         }
         if (hitCount > 0) {
@@ -140,6 +150,10 @@ public final class NbBreakpoint {
         updateValid(breakpoint, false);
         this.breakpoint = breakpoint;
         return CompletableFuture.completedFuture(this);
+    }
+
+    private static final String lsp2NBLogMessage(String message) {
+        return message.replaceAll("\\{([^\\}]+)\\}", "{=$1}");      // NOI18N
     }
 
     private void updateValid(Breakpoint breakpoint, boolean sendNotify) {
@@ -196,7 +210,7 @@ public final class NbBreakpoint {
             if (breakpoint instanceof LineBreakpoint) {
                 ((LineBreakpoint) breakpoint).setPrintText(logMessage);
             } else {
-                // no print text
+                ((TruffleLineBreakpoint) breakpoint).setPrintText(logMessage);
             }
         }
     }

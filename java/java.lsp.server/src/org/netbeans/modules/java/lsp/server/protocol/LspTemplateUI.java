67fcff9302a672230ce1f5e5fb2e5312e97b8b60
Svata Dedic <svatopluk.dedic@oracle.com>
2021-11-04 11:32:45 +0100

    Remove mavenism from LSP.

--- java/java.lsp.server/src/org/netbeans/modules/java/lsp/server/protocol/LspTemplateUI.java
+++ java/java.lsp.server/src/org/netbeans/modules/java/lsp/server/protocol/LspTemplateUI.java
@@ -138,10 +138,6 @@ abstract class LspTemplateUI {
                 final DataFolder target = targetAndName.first();
                 final String name = targetAndName.second();
                 Map<String,String> prjParams = new HashMap<>();
-                // PENDING: this is a mavenism; 
-                prjParams.put("version", "1.0-SNAPSHOT"); // NOI18N
-                prjParams.put("artifactId", name);  // NOI18N
-                prjParams.put("groupId", findGroupId(pkg, name));
                 prjParams.put("package", pkg);
                 prjParams.put("packageBase", pkg);
                 DataObject newObject = template.createFromTemplate(target, name, prjParams);

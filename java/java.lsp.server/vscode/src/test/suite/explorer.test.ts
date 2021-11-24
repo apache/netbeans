ea858daa364ec021011b927b21dcbaa101a91e3c
Svata Dedic <svatopluk.dedic@oracle.com>
2021-11-24 14:07:55 +0100

    License headers added.

--- java/java.lsp.server/vscode/src/test/suite/explorer.test.ts
+++ java/java.lsp.server/vscode/src/test/suite/explorer.test.ts
@@ -1,9 +1,25 @@
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import * as assert from 'assert';
 import * as fs from 'fs';
-import * as path from 'path';
-import * as ps from 'ps-node';
-import { spawn, ChildProcessByStdio, spawnSync, SpawnSyncReturns } from 'child_process';
-import { Readable } from 'stream';
 import * as Mocha from 'mocha';
 
 
@@ -12,7 +28,6 @@ import * as Mocha from 'mocha';
 import * as vscode from 'vscode';
 import * as myExtension from '../../extension';
 import * as myExplorer from '../../explorer';
-import { TextDocument, TextEditor, Uri } from 'vscode';
 import { assertWorkspace, dumpJava, prepareProject } from './testutils';
 
 Mocha.before(async () => {

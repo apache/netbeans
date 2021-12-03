adf74ef9bbb732dc77f1ec589c1083245d70cd4a
Martin Entlicher <martin.entlicher@oracle.com>
2021-12-03 11:50:45 +0100

    Run/Debug via project actions.

--- java/java.lsp.server/vscode/src/extension.ts
+++ java/java.lsp.server/vscode/src/extension.ts
@@ -927,7 +927,6 @@ class NetBeansConfigurationInitialProvider implements vscode.DebugConfigurationP
                     name: cname,
                     type: "java8+",
                     request: "launch",
-                    mainClass: '${file}',
                     launchConfiguration: cn,
                 };
                 result.push(debugConfig);
@@ -1005,9 +1004,7 @@ class NetBeansConfigurationResolver implements vscode.DebugConfigurationProvider
         if (!config.request) {
             config.request = 'launch';
         }
-        if ('launch' == config.request && !config.mainClass) {
-            config.mainClass = '${file}';
-        }
+        config.file = '${file}';
         if (!config.classPaths) {
             config.classPaths = ['any'];
         }

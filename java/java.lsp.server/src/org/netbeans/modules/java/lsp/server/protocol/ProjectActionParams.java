e9e2da4b05becfc5705c251e9184ba3d669cc6f1
Svata Dedic <svatopluk.dedic@oracle.com>
2021-12-12 15:52:59 +0100

    Fixed javadoc.

--- java/java.lsp.server/src/org/netbeans/modules/java/lsp/server/protocol/ProjectActionParams.java
+++ java/java.lsp.server/src/org/netbeans/modules/java/lsp/server/protocol/ProjectActionParams.java
@@ -21,9 +21,19 @@ package org.netbeans.modules.java.lsp.server.protocol;
 import org.eclipse.lsp4j.jsonrpc.validation.NonNull;
 import org.eclipse.xtext.xbase.lib.Pure;
 import org.eclipse.xtext.xbase.lib.util.ToStringBuilder;
+import org.netbeans.spi.project.ActionProvider;
+import org.netbeans.spi.project.ProjectConfiguration;
+import org.openide.filesystems.FileObject;
 
 /**
- * Describes 1st command parameter for 'java.project.action' generic command.
+ * Describes 1st command parameter for 'java.project.action' generic command. The 
+ * {@link #getAction()} is the project action ID that should be executed, like {@link ActionProvider#COMMAND_BUILD}.
+ * The client may specify a {@link ProjectConfiguration} to use for action's execution. By default, the active
+ * (or default) configuration is used if {@link #getFallbackDefault()} is enabled.
+ * <p>
+ * Other parameters can be passed to {@code java.project.run.action} LSP commands: it tries to interpret
+ * the 2nd and further arguments as URI strings, converts them into {@link FileObject}s and passes in context Lookup
+ * to the action.
  * @author sdedic
  */
 public final class ProjectActionParams {

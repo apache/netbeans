d87f916ed1958933aa20a792392bd55d0ded46ce
BradWalker <bwalker@musings.com>
2022-03-19 17:07:29 -0400

    Cleanup a lot of warnings that deal with LinkedList raw types.
       [repeat] /home/bwalker/src/netbeans/java/dbschema/src/org/netbeans/modules/dbschema/jdbcimpl/SchemaElementImpl.java:316: warning: [rawtypes] found raw type: LinkedList
       [repeat]         LinkedList result = new LinkedList<>();
       [repeat]         ^
       [repeat]   missing type arguments for generic class LinkedList<E>
       [repeat]   where E is a type-variable:
    
    There are a few remaining ones that I did not touch. These are related to the methods being public and requiring changes or instances where
    I would have to do major code work.

--- java/dbschema/src/org/netbeans/modules/dbschema/jdbcimpl/TableElementImpl.java
+++ java/dbschema/src/org/netbeans/modules/dbschema/jdbcimpl/TableElementImpl.java
@@ -597,7 +597,7 @@ public class TableElementImpl extends DBElementImpl implements TableElement.Impl
                         i++;
                     }
 
-                    LinkedList idxs = new LinkedList<>();
+                    LinkedList<String> idxs = new LinkedList<>();
                     for (Iterator it = cols.values().iterator(); it.hasNext();) {
                         // non-unique = false, thus the index is unique -- see initIndexes()
                         idxs.add(indexName + "." + it.next() + ".false"); // NOI18N

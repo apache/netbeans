9f44f35a04433a48cbd77dd2f6be22c8ca57dcef
Neil C Smith <neilcsmith@apache.org>
2022-04-12 15:52:58 +0100

    Switch to brandable and non-hidden default download directory for JDKs.
    Move the default Disco download directory to a non-hidden location. Downloading
    to a hidden location makes reregistering difficult and hides disk usage.
    
    The default directory name is brandable and defaults to NetBeansJDKs, mirroring
    the default projects location in OpenProjectListSettings::getProjectsFolder.
    Similarly uses tmp folder during development.

--- java/java.disco/src/org/netbeans/modules/java/disco/DiscoPlatformInstall.java
+++ java/java.disco/src/org/netbeans/modules/java/disco/DiscoPlatformInstall.java
@@ -18,15 +18,19 @@
  */
 package org.netbeans.modules.java.disco;
 
+import java.io.File;
+import javax.swing.filechooser.FileSystemView;
 import org.netbeans.spi.java.platform.CustomPlatformInstall;
 import org.openide.WizardDescriptor;
 import org.openide.WizardDescriptor.InstantiatingIterator;
+import org.openide.filesystems.FileUtil;
 import org.openide.util.NbBundle;
 
 @NbBundle.Messages({
     "DiscoPlatformInstall.displayName=Download OpenJDK (via Foojay.io Disco API)",
     "Disco.clientName=Apache NetBeans",
-    "Disco.defaultDistribution=Zulu"
+    "Disco.defaultDistribution=Zulu",
+    "Disco.defaultDownloadDir=NetBeansJDKs"
 })
 public class DiscoPlatformInstall extends CustomPlatformInstall {
 
@@ -53,6 +57,28 @@ public class DiscoPlatformInstall extends CustomPlatformInstall {
 
     static String defaultDistribution() {
         return Bundle.Disco_defaultDistribution();
+    }
+
+    // @TODO - this uses similar logic to OpenProjectListSettings::getProjectsFolder
+    // but should use preferences to store the last used location
+    static File getDefaultDownloadFolder() {
+        String dirName = Bundle.Disco_defaultDownloadDir();
+        File dir;
+        if (Boolean.getBoolean("netbeans.full.hack")) { // NOI18N
+            File tmp = new File(System.getProperty("java.io.tmpdir", "")); // NOI18N
+            dir = new File(tmp, dirName);
+        } else {
+            File parent = FileSystemView.getFileSystemView().getDefaultDirectory();
+            if (parent != null && parent.isDirectory()) {
+                dir = new File(parent, dirName);
+            } else {
+                dir = new File(System.getProperty("user.home", ""), dirName); // NOI18N
+            }
+        }
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
+        return FileUtil.normalizeFile(dir);
     }
 
 }
--- java/java.disco/src/org/netbeans/modules/java/disco/OS.java
+++ java/java.disco/src/org/netbeans/modules/java/disco/OS.java
@@ -21,7 +21,6 @@ package org.netbeans.modules.java.disco;
 import eu.hansolo.jdktools.Architecture;
 import eu.hansolo.jdktools.OperatingSystem;
 import java.io.File;
-import org.checkerframework.checker.nullness.qual.Nullable;
 import org.openide.util.Utilities;
 
 public class OS {
@@ -48,51 +47,8 @@ public class OS {
         return arch;
     }
 
-    @Nullable
     public static File getDefaultDownloadFolder() {
-        String home = System.getProperty("user.home");
-        if (home == null)
-            return null;
-
-        switch (getOperatingSystem()) {
-            case MACOS:
-                // macOS: Library/Java/JavaVirtualMachines
-                File library = new File(home, "Library");
-                if (library.exists() && library.isDirectory()) {
-                    File d = new File(new File(library, "Java"), "JavaVirtualMachines");
-                    d.mkdirs();
-                    return d;
-                }
-                break;
-            case WINDOWS:
-                // Windows: %LOCALAPPDATA%/Java or AppData/Local
-                String local = System.getenv("LOCALAPPDATA");
-                if (local != null) {
-                    File appdata = new File(local);
-                    if (appdata.exists()) {
-                        File d = new File(appdata, "Java");
-                        d.mkdirs();
-                        return d;
-                    }
-                }
-                break;
-            case LINUX:
-                // Linux: $XDG_DATA_HOME/jvm/ or $HOME/.local/share
-                String xdg = System.getenv("XDG_DATA_HOME");
-                if (xdg != null) {
-                    File xdgData = new File(xdg);
-                    if (xdgData.exists()) {
-                        File d = new File(xdgData, "jvm");
-                        d.mkdirs();
-                        return d;
-                    }
-                }
-                //fallback
-                File d = new File(home, ".local/share/jvm");
-                d.mkdirs();
-                return d;
-        }
-        return null;
+        return DiscoPlatformInstall.getDefaultDownloadFolder();
     }
 
 }

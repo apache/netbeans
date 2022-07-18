a3f61db61845bcf133c7a4167e391ca5db0b7282
José Contreras <pepness@apache.org>
2022-07-17 22:48:14 -0500

    - Add mdd files for Jakarta EE 9 schemas - Add DataLoader for Jakarta EE 9 schemas - Add DataLoader for Java EE 8 schemas - Add EJB 4.0 xml descriptor

---
+++ enterprise/j2ee.ddloaders/src/org/netbeans/modules/j2ee/ddloaders/web/DDWebFragment50DataLoader.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.netbeans.modules.j2ee.ddloaders.web;
+
+import java.io.IOException;
+import org.openide.filesystems.FileObject;
+import org.openide.loaders.DataObjectExistsException;
+import org.openide.loaders.MultiDataObject;
+
+/**
+ * A data loader for web-fragment.xml version 5.0. Required for providing
+ * a different action context than for older versions - see #85570.
+ * 
+ * @author pepness
+ */
+public class DDWebFragment50DataLoader extends DDDataLoader {
+
+    private static final long serialVersionUID = 1L;
+
+    public static final String REQUIRED_MIME_50 = "text/x-dd-servlet-fragment5.0"; // NOI18N
+
+    public DDWebFragment50DataLoader() {
+        super("org.netbeans.modules.j2ee.ddloaders.web.DDFragmentDataObject");  // NOI18N
+    }
+
+    @Override
+    protected String actionsContext() {
+        return "Loaders/text/x-dd-web-fragment5.0/Actions/"; // NOI18N
+    }
+
+    @Override
+    protected String[] getSupportedMimeTypes() {
+        return new String[]{REQUIRED_MIME_50};
+    }
+
+    @Override
+    protected MultiDataObject createMultiObject(FileObject primaryFile) throws DataObjectExistsException, IOException {
+        return new DDFragmentDataObject(primaryFile, this, REQUIRED_MIME_50);
+    }
+
+}

c212087b01a00579a1a5c1a9d4400f9071017f56
meghna_jayan <meghna.jayan@oracle.com>
2022-08-24 21:51:55 +0530

    Add autocompletion for Record Patterns

--- java/java.completion/src/org/netbeans/modules/java/completion/JavaCompletionTask.java
+++ java/java.completion/src/org/netbeans/modules/java/completion/JavaCompletionTask.java
@@ -57,6 +57,7 @@ import static javax.lang.model.SourceVersion.RELEASE_10;
 import static javax.lang.model.SourceVersion.RELEASE_11;
 import static javax.lang.model.SourceVersion.RELEASE_13;
 import static javax.lang.model.type.TypeKind.VOID;
+import org.netbeans.modules.java.completion.impl.TreeShims;
 
 /**
  *
@@ -126,6 +127,10 @@ public final class JavaCompletionTask<T> extends BaseTask {
     
     public static interface ModuleItemFactory<T> extends ItemFactory<T> {
         T createModuleItem(String moduleName, int substitutionOffset);
+    }
+
+    public static interface RecordPatternItemFactory<T> extends ItemFactory<T> {
+        T createRecordPatternItem(CompilationInfo info, TypeElement elem, DeclaredType type, int substitutionOffset, ReferencesCount referencesCount, boolean isDeprecated, boolean insideNew, boolean addTypeVars);
     }
 
     public static enum Options {
@@ -489,6 +494,11 @@ public final class JavaCompletionTask<T> extends BaseTask {
             case DEFAULT_CASE_LABEL:
                 localResult(env);
                 addKeywordsForBlock(env);
+                break;
+            default:
+                if (path.getLeaf().getKind().toString().equals(TreeShims.DECONSTRUCTION_PATTERN)) {
+                    insideDeconstructionRecordPattern(env);
+                }
                 break;
         }
     }
@@ -3238,6 +3248,16 @@ public final class JavaCompletionTask<T> extends BaseTask {
 
     }
 
+    private void insideDeconstructionRecordPattern(final Env env) throws IOException {
+        final CompilationController controller = env.getController();
+        final Elements elements = controller.getElements();
+        TypeMirror tm = controller.getTreeUtilities().parseType(env.getPrefix(), env.getScope().getEnclosingClass());
+        TypeElement e = (TypeElement) ((DeclaredType) tm).asElement();
+        if (e.getSimpleName().toString().contentEquals(env.getPrefix()) && (e.getKind().equals(ElementKind.RECORD))) {
+            results.add(((RecordPatternItemFactory<T>) itemFactory).createRecordPatternItem(env.getController(), e, (DeclaredType) e.asType(), anchorOffset, null, elements.isDeprecated(e), env.isInsideNew(), env.isInsideNew() || env.isInsideClass()));
+        }
+    }
+
     private void localResult(Env env) throws IOException {
         addLocalMembersAndVars(env);
         addClassTypes(env, null);
@@ -4170,7 +4190,14 @@ public final class JavaCompletionTask<T> extends BaseTask {
             }
         };
         for (TypeElement e : controller.getElementUtilities().getGlobalTypes(acceptor)) {
-            results.add(itemFactory.createTypeItem(env.getController(), e, (DeclaredType) e.asType(), anchorOffset, null, elements.isDeprecated(e), env.isInsideNew(), env.isInsideNew() || env.isInsideClass(), false, false, false));
+            Tree iot = env.getPath().getLeaf();
+            TokenSequence<JavaTokenId> ts = findLastNonWhitespaceToken(env, iot, env.getOffset());
+            if (env.getPrefix() != null && e.getSimpleName().toString().contentEquals(env.getPrefix()) && (e.getKind().equals(ElementKind.RECORD))
+                    && ts != null && ts.token().id() == JavaTokenId.INSTANCEOF) {
+                results.add(((RecordPatternItemFactory<T>) itemFactory).createRecordPatternItem(controller, e, (DeclaredType) e.asType(), anchorOffset, null, controller.getElements().isDeprecated(e), env.isInsideNew(), env.isInsideNew() || env.isInsideClass()));
+            } else {
+                results.add(itemFactory.createTypeItem(env.getController(), e, (DeclaredType) e.asType(), anchorOffset, null, elements.isDeprecated(e), env.isInsideNew(), env.isInsideNew() || env.isInsideClass(), false, false, false));
+            }
         }
     }
 

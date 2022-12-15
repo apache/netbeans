8769e00c19d6de0f51df33d71ca802e4dde5505e
Lukasz Bownik <lukasz.bownik@gmail.com>
2022-12-15 14:16:01 +0100

    Added more tests BaseUtilities Added more tests to Pair class

--- platform/openide.util/test/unit/src/org/openide/util/PairTest.java
+++ platform/openide.util/test/unit/src/org/openide/util/PairTest.java
@@ -19,39 +19,90 @@
 package org.openide.util;
 
 import org.netbeans.junit.NbTestCase;
-import org.openide.util.Pair;
 
 /**
  *
- * @author Tomas Zezula
+ * @author Tomas Zezula, Lukasz Bownik
  */
 public class PairTest extends NbTestCase {
 
-    public PairTest(String name) {
+    //--------------------------------------------------------------------------
+    public PairTest(final String name) {
+
         super(name);
     }
 
+    //-------------------------------------------------------------------------- 
+    public void test_pairEquality() {
 
-    public void testPairs() {
-        final Pair<Integer,Integer> p1a = Pair.of(1, 1);
-        final Pair<Integer,Integer> p1b = Pair.of(1, 1);
-        final Pair<Integer,Integer> p2 = Pair.of(1, 2);
-        final Pair<Integer,Integer> p3 = Pair.of(2, 1);
-        final Pair<Integer,Integer> p4 = Pair.of(null, 1);
-        final Pair<Integer,Integer> p5 = Pair.of(1, null);
-        final Pair<Integer,Integer> p6 = Pair.of(null, null);
-        assertTrue(p1a.equals(p1a));
-        assertTrue(p1a.equals(p1b));
-        assertFalse(p1a.equals(p2));
-        assertFalse(p1a.equals(p3));
-        assertFalse(p1a.equals(p4));
-        assertFalse(p1a.equals(p5));
-        assertFalse(p1a.equals(p6));
-        assertEquals(p1a.hashCode(), p1b.hashCode());
-        assertEquals(p4.hashCode(), p4.hashCode());
-        assertEquals(p5.hashCode(), p5.hashCode());
-        assertEquals(p6.hashCode(), p6.hashCode());
-        assertFalse(p4.hashCode() == p5.hashCode());
+        Pair<Integer, Integer> pair11a = Pair.of(1, 1);
+        Pair<Integer, Integer> pair11b = Pair.of(1, 1);
+        Pair<Integer, Integer> pair12 = Pair.of(1, 2);
+        Pair<Integer, Integer> pairNul1 = Pair.of(null, 1);
+        Pair<Integer, Integer> pair1Null = Pair.of(1, null);
+        Pair<Integer, Integer> pairNullNull = Pair.of(null, null);
+
+        assertTrue(pair11a.equals(pair11a));
+        assertEquals(pair11a.hashCode(), pair11a.hashCode());
+        assertEquals(new Integer(1), pair11a.first());
+        assertEquals(new Integer(1), pair11a.second());
+
+        assertTrue(pair12.equals(pair12));
+        assertEquals(pair12.hashCode(), pair12.hashCode());
+        assertEquals(new Integer(1), pair12.first());
+        assertEquals(new Integer(2), pair12.second());
+
+        assertTrue(pair11a.equals(pair11b));
+        assertEquals(pair11a.hashCode(), pair11b.hashCode());
+        assertEquals(new Integer(1), pair11b.first());
+        assertEquals(new Integer(1), pair11b.second());
+
+        assertTrue(pair1Null.equals(pair1Null));
+        assertEquals(pair1Null.hashCode(), pair1Null.hashCode());
+        assertEquals(new Integer(1), pair1Null.first());
+        assertEquals(null, pair1Null.second());
+
+        assertTrue(pairNul1.equals(pairNul1));
+        assertEquals(pairNul1.hashCode(), pairNul1.hashCode());
+        assertEquals(null, pairNul1.first());
+        assertEquals(new Integer(1), pairNul1.second());
+
+        assertTrue(pairNullNull.equals(pairNullNull));
+        assertEquals(pairNullNull.hashCode(), pairNullNull.hashCode());
+        assertEquals(null, pairNullNull.first());
+        assertEquals(null, pairNullNull.second());
     }
 
+    //-------------------------------------------------------------------------- 
+    public void test_pairInequality() {
+
+        Pair<Integer, Integer> pair11a = Pair.of(1, 1);
+        Pair<Integer, Integer> pair12 = Pair.of(1, 2);
+        Pair<Integer, Integer> pair21 = Pair.of(2, 1);
+        Pair<Integer, Integer> pairNul1 = Pair.of(null, 1);
+        Pair<Integer, Integer> pair1Null = Pair.of(1, null);
+        Pair<Integer, Integer> pairNullNull = Pair.of(null, null);
+        
+        assertFalse(pair11a.equals(pair12));
+        assertFalse(pair11a.equals(pair21));
+        assertFalse(pair11a.equals(pairNul1));
+        assertFalse(pair11a.equals(pair1Null));
+        assertFalse(pair11a.equals(pairNullNull));
+        assertFalse(pairNul1.equals(pair11a));
+        assertFalse(pair1Null.equals(pair11a));
+        assertFalse(pairNullNull.equals(pair11a));
+
+        assertFalse(pair11a.equals(null));
+        assertFalse(pair11a.equals(""));
+    }
+
+    //--------------------------------------------------------------------------
+    public void test_pair_toString() {
+        
+        assertEquals("Pair[1,1]", Pair.of(1, 1).toString());
+        assertEquals("Pair[1,2]", Pair.of(1, 2).toString());
+        assertEquals("Pair[1,null]", Pair.of(1, null).toString());
+        assertEquals("Pair[null,1]", Pair.of(null, 1).toString());
+        assertEquals("Pair[null,null]", Pair.of(null, null).toString());
+    }
 }

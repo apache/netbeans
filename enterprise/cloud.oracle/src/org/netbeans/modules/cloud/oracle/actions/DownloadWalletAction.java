d01c43d8835d24ddaf85ffbcad1390c32fcf28d1
Svata Dedic <svatopluk.dedic@oracle.com>
2023-01-25 13:53:18 +0100

    Proper escaping of TNS_ADMIN path. Select JDBC driver with defined implementation.

--- enterprise/cloud.oracle/src/org/netbeans/modules/cloud/oracle/actions/DownloadWalletAction.java
+++ enterprise/cloud.oracle/src/org/netbeans/modules/cloud/oracle/actions/DownloadWalletAction.java
@@ -19,14 +19,16 @@
 package org.netbeans.modules.cloud.oracle.actions;
 
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.text.MessageFormat;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
@@ -45,6 +47,8 @@ import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
 import org.openide.awt.ActionRegistration;
 import org.openide.awt.StatusDisplayer;
+import org.openide.filesystems.URLMapper;
+import org.openide.util.BaseUtilities;
 import org.openide.util.ContextAwareAction;
 import org.openide.util.Exceptions;
 import org.openide.util.Lookup;
@@ -75,8 +79,9 @@ import org.openide.util.NbBundle;
     "MSG_WalletNoConnection=Wallet doesn't contain any connection"
 })
 public class DownloadWalletAction extends AbstractAction implements ContextAwareAction {
+    private static final Logger LOG = Logger.getLogger(DownloadWalletAction.class.getName());
     
-    private static final String URL_TEMPLATE = "jdbc:oracle:thin:@{0}?TNS_ADMIN=\"{1}\""; //NOI18N
+    private static final String URL_TEMPLATE = "jdbc:oracle:thin:@{0}?TNS_ADMIN={1}"; //NOI18N
     private final DatabaseItem context;
     private OCIProfile session;
 
@@ -105,7 +110,22 @@ public class DownloadWalletAction extends AbstractAction implements ContextAware
                 if (p.getDbUser() != null && p.getDbPassword() != null) {
                     
                     JDBCDriver[] drivers = JDBCDriverManager.getDefault().getDrivers("oracle.jdbc.OracleDriver"); //NOI18N
+                    JDBCDriver jarsPresent = null;
+                    
                     if (drivers.length > 0) {
+                        
+                        // prefer a driver that actually defines some JARs.
+                        for (JDBCDriver d : drivers) {
+                            if (isAvailable(d)) {
+                                jarsPresent = d;
+                                break;
+                            }
+                        }
+                        if (jarsPresent == null) {
+                            jarsPresent = drivers[0];
+                            // PENDING: should be shown to the user ?
+                            LOG.log(Level.WARNING, "Unable to find driver JARs for wallet {0}, using fallback driver: {1}", new Object[] { walletPath, jarsPresent.getName() });
+                        }
                         String connectionName = context.getConnectionName();
                         if (connectionName == null) {
                             Optional<String> n = parseConnectionNames(walletPath).stream().findFirst();
@@ -116,7 +136,7 @@ public class DownloadWalletAction extends AbstractAction implements ContextAware
                                 return;
                             }
                         }
-                        String dbUrl = MessageFormat.format(URL_TEMPLATE, connectionName, walletPath);
+                        String dbUrl = MessageFormat.format(URL_TEMPLATE, connectionName, BaseUtilities.escapeParameters(new String[] { walletPath.toString() }));
                         DatabaseConnection dbConn = DatabaseConnection.create(
                                 drivers[0], 
                                 dbUrl, 
@@ -127,6 +147,9 @@ public class DownloadWalletAction extends AbstractAction implements ContextAware
                                 context.getName());
                         ConnectionManager.getDefault().addConnection(dbConn);
                     }
+                    
+                    // PENDING: what should happen, if the driver is not found at all - display an info message ?
+                    
                     DialogDisplayer.getDefault().notifyLater(
                             new NotifyDescriptor.Message(
                                     Bundle.MSG_WalletDownloadedPassword(
@@ -140,6 +163,29 @@ public class DownloadWalletAction extends AbstractAction implements ContextAware
         });
     }
     
+    static boolean isAvailable(JDBCDriver driver) {
+        URL[] urls = driver.getURLs();
+        for (URL u : urls) {
+            if (URLMapper.findFileObject(u) == null) {
+                return false;
+            }
+        }
+        if (urls.length > 0) {
+            // true, some jar is defined && exists.
+            return true;
+        } else {
+            // if the JDBC drive does not list jars, its class must be reachable. DbDriverManager uses no-arg classloader constructor, so it is
+            // using systemClassLoader as a parent for no-URL URLClassloader.
+            try {
+                Class.forName(driver.getClassName(), true, ClassLoader.getSystemClassLoader());
+                return true;
+            } catch (ClassNotFoundException | SecurityException | LinkageError ex) {
+                // expected, class is not avaialble
+                return false;
+            }
+        }
+    }
+
     protected List<String> parseConnectionNames(Path wallet) {
         Path tns = wallet.resolve("tnsnames.ora"); //NOI18N
         try {

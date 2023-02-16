34c52bf1ebeb1ea5722c14090c1e76a70566fcbb
Andrey Bruykhov <andreybr611@gmail.com>
2023-02-16 16:52:04 +0300

    javafx: removed unused private classes

--- javafx/javafx2.editor/src/org/netbeans/modules/javafx2/editor/actions/FieldsWithTypeParams.java
+++ javafx/javafx2.editor/src/org/netbeans/modules/javafx2/editor/actions/FieldsWithTypeParams.java
@@ -23,19 +23,7 @@ import java.util.Map;
 
 public class FieldsWithTypeParams {
 
-    private class SimpleTypeParam<T> {
-        // something
-    }
-
-    private class BoundParam<T extends Comparable & Runnable> {
-
-    }
-
     private class NestedTypeParam<K, V, T extends Map<K, V>> {
-
-    }
-
-    private class ExtendsTyped<T, V> extends HashMap<T, V> {
 
     }
 

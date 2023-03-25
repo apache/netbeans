81c25719953416a62de7ac1e94e0ade18396a390
Eirik Bakke <ebakke@ultorg.com>
2023-03-25 17:50:10 -0400

    Replace calls to KeyEvent.getKeyText with Actions.keyStrokeToString where safe/appropriate.
    The Actions.keyStrokeToString method shows more user-friendly keyboard shortcut strings on MacOS, and in some other cases. This commit replaces calls to KeyEvent.getKeyText with Actions.keyStrokeToString in various places around the NetBeans codebase, for cases where I'm reasonably sure that the resulting string is used only for UI display purposes.
    
    The options.keymap module has not been touched, as this module probably makes assumptions about the format of the generated shortcut strings, e.g. for persistence of keymap configuration. Displaying better shortcut strings on MacOS in the Keymap options pane is better left as a separate project.
    
    For this commit, all occurrences of the following strings were reviewed, via "git grep":
    * KeyEvent.getKeyText
    * KeyEvent.getKeyModifiersText
    * InputEvent.getModifiersExText

--- harness/jellytools.platform/src/org/netbeans/jellytools/actions/Action.java
+++ harness/jellytools.platform/src/org/netbeans/jellytools/actions/Action.java
@@ -23,7 +23,6 @@ import java.awt.Container;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 import javax.swing.KeyStroke;
 import javax.swing.tree.TreePath;
 
@@ -44,6 +43,7 @@ import org.netbeans.jemmy.operators.Operator.ComponentVisualizer;
 import org.netbeans.jemmy.operators.Operator.DefaultStringComparator;
 import org.netbeans.jemmy.operators.Operator.StringComparator;
 import org.netbeans.jemmy.util.EmptyVisualizer;
+import org.openide.awt.Actions;
 import org.openide.cookies.InstanceCookie;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
@@ -1281,8 +1281,8 @@ public class Action {
          */
         @Override
         public String toString() {
-            String s = KeyEvent.getKeyModifiersText(getKeyModifiers());
-            return s + (s.length() > 0 ? "+" : "") + KeyEvent.getKeyText(getKeyCode());
+            return Actions.keyStrokeToString(
+                    KeyStroke.getKeyStroke(getKeyCode(), getKeyModifiers()));
         }
     }
 }

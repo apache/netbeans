c78397b737936bb42c9c6c05b5964065553641f1
Svata Dedic <svatopluk.dedic@oracle.com>
2023-04-18 14:12:25 +0200

    Treat failed assumeXXX as test success in JUnit3-style tests.

--- harness/nbjunit/src/org/netbeans/junit/NbTestCase.java
+++ harness/nbjunit/src/org/netbeans/junit/NbTestCase.java
@@ -61,6 +61,7 @@ import java.util.regex.Pattern;
 import junit.framework.AssertionFailedError;
 import junit.framework.TestCase;
 import junit.framework.TestResult;
+import org.junit.AssumptionViolatedException;
 import org.junit.Ignore;
 import org.netbeans.insane.live.LiveReferences;
 import org.netbeans.insane.live.Path;
@@ -74,6 +75,7 @@ import org.netbeans.junit.internal.NbModuleLogHandler;
  * Adds various abilities such as comparing golden files, getting a working
  * directory for test files, testing memory usage, etc.
  */
+
 public abstract class NbTestCase extends TestCase implements NbTest {
     static {
         MethodOrder.initialize();
@@ -474,6 +476,8 @@ public abstract class NbTestCase extends TestCase implements NbTest {
                     long now = System.nanoTime();
                     try {
                         runTest();
+                    } catch (AssumptionViolatedException ex) {
+                        // ignore, the test is assumed to be meaningless.
                     } catch (Throwable t) {
                         noteWorkDir(workdirNoCreate());
                         throw noteRandomness(t);

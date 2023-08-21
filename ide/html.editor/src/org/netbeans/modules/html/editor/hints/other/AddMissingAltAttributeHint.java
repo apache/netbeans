a84cc678b049023775443225cbb1429059142170
Christian Lenz <christian.lenz@gmx.net>
2023-08-21 21:55:01 +0200

    Add AltAttributeVisitor to find the correct tag and attribute to show the hint

---
+++ ide/html.editor/src/org/netbeans/modules/html/editor/hints/other/AddMissingAltAttributeHint.java
@@ -0,0 +1,90 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.netbeans.modules.html.editor.hints.other;
+
+import java.awt.EventQueue;
+import java.util.Collections;
+import javax.swing.text.BadLocationException;
+import org.netbeans.modules.csl.api.Hint;
+import org.netbeans.modules.csl.api.HintFix;
+import org.netbeans.modules.csl.api.OffsetRange;
+import org.netbeans.modules.csl.api.Rule;
+import org.netbeans.modules.csl.api.RuleContext;
+import org.netbeans.modules.html.editor.hints.HtmlRuleContext;
+import org.netbeans.modules.html.editor.utils.HtmlTagContextUtils;
+import org.netbeans.modules.parsing.api.Source;
+import org.openide.util.Exceptions;
+
+/**
+ *
+ * @author Christian Lenz
+ */
+public class AddMissingAltAttributeHint extends Hint {
+
+    public AddMissingAltAttributeHint(HtmlRuleContext context, OffsetRange range) {
+        super(AddMissingAltAttributeRule.SINGLETON,
+            AddMissingAltAttributeRule.SINGLETON.getDescription(),
+            context.getFile(),
+            range,
+            Collections.<HintFix>singletonList(new AddMissingAltAttributeHintFix(context, range)),
+            10);
+    }
+
+    private static class AddMissingAltAttributeHintFix implements HintFix {
+
+        HtmlRuleContext context;
+        OffsetRange range;
+
+        public AddMissingAltAttributeHintFix(HtmlRuleContext context, OffsetRange range) {
+            this.context = context;
+            this.range = range;
+        }
+
+        @Override
+        public String getDescription() {
+            return AddMissingAltAttributeRule.SINGLETON.getDisplayName();
+        }
+
+        @Override
+        public void implement() throws Exception {
+            EventQueue.invokeLater(() -> {
+                try {
+                    Source source = Source.create(context.getFile());
+                    OffsetRange adjustContextRange = HtmlTagContextUtils.adjustContextRange(source.getDocument(false), range.getStart(), range.getEnd(), true);
+
+                    source.getDocument(false).insertString(adjustContextRange.getEnd(), " alt=\"\"", null);
+                } catch (BadLocationException ex) {
+                    Exceptions.printStackTrace(ex);
+                }
+            });
+
+        }
+
+        @Override
+        public boolean isSafe() {
+            return true;
+        }
+
+        @Override
+        public boolean isInteractive() {
+            return false;
+        }
+    }
+
+}
---
+++ ide/html.editor/src/org/netbeans/modules/html/editor/hints/other/AltAttributeVisitor.java
@@ -0,0 +1,105 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.netbeans.modules.html.editor.hints.other;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
+import javax.swing.text.Document;
+import org.netbeans.api.html.lexer.HTMLTokenId;
+import org.netbeans.api.lexer.Token;
+import org.netbeans.api.lexer.TokenHierarchy;
+import org.netbeans.api.lexer.TokenSequence;
+import org.netbeans.modules.csl.api.Hint;
+import org.netbeans.modules.csl.api.OffsetRange;
+import org.netbeans.modules.csl.api.Rule;
+import org.netbeans.modules.html.editor.api.Utils;
+import org.netbeans.modules.html.editor.hints.HtmlRuleContext;
+import org.netbeans.modules.html.editor.lib.api.elements.*;
+import org.netbeans.modules.html.editor.refactoring.InlinedStyleInfo;
+import org.netbeans.modules.parsing.api.Source;
+
+/**
+ *
+ * @author Christian Lenz
+ */
+public class AltAttributeVisitor implements ElementVisitor {
+    private static final String ALT_ATTR = "alt";
+
+    private final HtmlRuleContext context;
+    private final List<Hint> hints;
+    private final String tagToFindRegEx;
+
+    public AltAttributeVisitor(Rule rule, HtmlRuleContext context, List<Hint> hints, String tagToFindRegEx) throws IOException {
+        this.context = context;
+        this.hints = hints;
+        this.tagToFindRegEx = tagToFindRegEx;
+    }
+
+    @Override
+    public void visit(Element node) {
+        Source source = Source.create(context.getFile());
+        Document doc = source.getDocument(false);
+        final AtomicReference<List<InlinedStyleInfo>> result = new AtomicReference<>();
+
+        doc.render(() -> {
+            List<InlinedStyleInfo> found = new LinkedList<>();
+            result.set(found);
+
+            TokenHierarchy th = TokenHierarchy.get(doc);
+            TokenSequence<HTMLTokenId> ts = Utils.getJoinedHtmlSequence(th, node.from());
+
+            if (ts == null) {
+                return;
+            }
+
+            OffsetRange range;
+            String tag = null;
+            String attr = null;
+            int startTagOffset = 0;
+            int endTagOffset = 0;
+
+            do {
+                Token<HTMLTokenId> t = ts.token();
+                if (t.id() == HTMLTokenId.TAG_OPEN) {
+                    tag = t.text().toString();
+                    attr = null;
+
+                    startTagOffset = ts.offset();
+                } else if (t.id() == HTMLTokenId.TAG_CLOSE_SYMBOL) {
+                    endTagOffset = ts.offset();
+
+                    range = new OffsetRange(startTagOffset, endTagOffset);
+                    //closing tag, produce the info
+                    if (tag != null && tag.matches(tagToFindRegEx) && attr == null) {
+                        //alt attribute found
+                        hints.add(new AddMissingAltAttributeHint(context, range));
+
+                        tag = attr = null;
+                    }
+                } else if (t.id() == HTMLTokenId.ARGUMENT) {
+                    if (t.text().toString().equals(ALT_ATTR)) {
+                        attr = t.text().toString();
+                        range = null;
+                    }
+                }
+            } while (ts.moveNext() && ts.offset() <= node.to());
+        });
+    }
+}

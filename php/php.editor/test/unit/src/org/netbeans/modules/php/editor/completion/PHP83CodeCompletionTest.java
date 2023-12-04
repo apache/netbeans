be57ef0ad6f1d324cf87737d6070840aceb8fc1d
Junichi Yamamoto <junichi11@apache.org>
2023-12-05 07:19:45 +0900

    PHP 8.3 Support: Typed class constants (Part 5)
    - https://github.com/apache/netbeans/issues/6701
    - https://wiki.php.net/rfc/typed_class_constants
    - Fix the Code Completion feature
    - Add templates for constant statements
    - Add unit tests

--- php/php.editor/test/unit/src/org/netbeans/modules/php/editor/completion/PHP83CodeCompletionTest.java
+++ php/php.editor/test/unit/src/org/netbeans/modules/php/editor/completion/PHP83CodeCompletionTest.java
@@ -139,4 +139,349 @@ public class PHP83CodeCompletionTest extends PHPCodeCompletionTestBase {
     public void testDynamicClassConstantFetch_08e() throws Exception {
         checkCompletion("dynamicClassConstantFetch", "    case TEST4 = EnumTest::{self::{self::TES} . self::T^};");
     }
+
+    public void testTypedClassConstants_Class01() throws Exception {
+        checkCompletion("typedClassConstants", "    public const ^WITHOUT_TYPE = 1;");
+    }
+
+    public void testTypedClassConstants_Class02() throws Exception {
+        checkCompletion("typedClassConstants", "    const ?^A NULLABLE = null;");
+    }
+
+    public void testTypedClassConstants_Class03() throws Exception {
+        checkCompletion("typedClassConstants", "    public const ?in^t NULLABLE2 = 1;");
+    }
+
+    public void testTypedClassConstants_Class04a() throws Exception {
+        checkCompletion("typedClassConstants", "    private const ^A|B UNION = A;");
+    }
+
+    public void testTypedClassConstants_Class04b() throws Exception {
+        checkCompletion("typedClassConstants", "    private const A|^B UNION = A;");
+    }
+
+    public void testTypedClassConstants_Class04c() throws Exception {
+        checkCompletion("typedClassConstants", "    private const A|B^ UNION = A;");
+    }
+
+    public void testTypedClassConstants_Class05a() throws Exception {
+        checkCompletion("typedClassConstants", "    protected const ^A&B INTERSECTION = B;");
+    }
+
+    public void testTypedClassConstants_Class05b() throws Exception {
+        checkCompletion("typedClassConstants", "    protected const A^&B INTERSECTION = B;");
+    }
+
+    public void testTypedClassConstants_Class05c() throws Exception {
+        checkCompletion("typedClassConstants", "    protected const A&^B INTERSECTION = B;");
+    }
+
+    public void testTypedClassConstants_Class06a() throws Exception {
+        checkCompletion("typedClassConstants", "    public const (^A&B)|C DNF = C;");
+    }
+
+    public void testTypedClassConstants_Class06b() throws Exception {
+        checkCompletion("typedClassConstants", "    public const (A^&B)|C DNF = C;");
+    }
+
+    public void testTypedClassConstants_Class06c() throws Exception {
+        checkCompletion("typedClassConstants", "    public const (A&^B)|C DNF = C;");
+    }
+
+    public void testTypedClassConstants_Class06d() throws Exception {
+        checkCompletion("typedClassConstants", "    public const (A&B^)|C DNF = C;");
+    }
+
+    public void testTypedClassConstants_Class06e() throws Exception {
+        checkCompletion("typedClassConstants", "    public const (A&B)|^C DNF = C;");
+    }
+
+    public void testTypedClassConstants_Class06f() throws Exception {
+        checkCompletion("typedClassConstants", "    public const (A&B)|C^ DNF = C;");
+    }
+
+    public void testTypedClassConstants_Class07() throws Exception {
+        checkCompletion("typedClassConstants", "    const ?A NULLABLE = ^null;");
+    }
+
+    public void testTypedClassConstants_Interface01() throws Exception {
+        checkCompletion("typedClassConstants", "    public const ^WITHOUT_TYPE = 1; // interface");
+    }
+
+    public void testTypedClassConstants_Interface02() throws Exception {
+        checkCompletion("typedClassConstants", "    public const ?^A NULLABLE = null;  // interface");
+    }
+
+    public void testTypedClassConstants_Interface03() throws Exception {
+        checkCompletion("typedClassConstants", "    public const ?in^t NULLABLE2 = 1; // interface");
+    }
+
+    public void testTypedClassConstants_Interface04a() throws Exception {
+        checkCompletion("typedClassConstants", "    public const ^A|B UNION = A; // interface");
+    }
+
+    public void testTypedClassConstants_Interface04b() throws Exception {
+        checkCompletion("typedClassConstants", "    public const A^|B UNION = A; // interface");
+    }
+
+    public void testTypedClassConstants_Interface04c() throws Exception {
+        checkCompletion("typedClassConstants", "    public const A|^B UNION = A; // interface");
+    }
+
+    public void testTypedClassConstants_Interface04d() throws Exception {
+        checkCompletion("typedClassConstants", "    public const A|B^ UNION = A; // interface");
+    }
+
+    public void testTypedClassConstants_Interface05a() throws Exception {
+        checkCompletion("typedClassConstants", "    public const ^A&B INTERSECTION = B; // interface");
+    }
+
+    public void testTypedClassConstants_Interface05b() throws Exception {
+        checkCompletion("typedClassConstants", "    public const A^&B INTERSECTION = B; // interface");
+    }
+
+    public void testTypedClassConstants_Interface05c() throws Exception {
+        checkCompletion("typedClassConstants", "    public const A&^B INTERSECTION = B; // interface");
+    }
+
+    public void testTypedClassConstants_Interface05d() throws Exception {
+        checkCompletion("typedClassConstants", "    public const A&B^ INTERSECTION = B; // interface");
+    }
+
+    public void testTypedClassConstants_Interface06a() throws Exception {
+        checkCompletion("typedClassConstants", "    public const (^A&B)|C DNF = C; // interface");
+    }
+
+    public void testTypedClassConstants_Interface06b() throws Exception {
+        checkCompletion("typedClassConstants", "    public const (A^&B)|C DNF = C; // interface");
+    }
+
+    public void testTypedClassConstants_Interface06c() throws Exception {
+        checkCompletion("typedClassConstants", "    public const (A&^B)|C DNF = C; // interface");
+    }
+
+    public void testTypedClassConstants_Interface06d() throws Exception {
+        checkCompletion("typedClassConstants", "    public const (A&B^)|C DNF = C; // interface");
+    }
+
+    public void testTypedClassConstants_Interface06e() throws Exception {
+        checkCompletion("typedClassConstants", "    public const (A&B)|^C DNF = C; // interface");
+    }
+
+    public void testTypedClassConstants_Interface06f() throws Exception {
+        checkCompletion("typedClassConstants", "    public const (A&B)|C^ DNF = C; // interface");
+    }
+
+    public void testTypedClassConstants_Interface07() throws Exception {
+        checkCompletion("typedClassConstants", "    public const A|B UNION = ^A; // interface");
+    }
+
+    public void testTypedClassConstants_Trait01() throws Exception {
+        checkCompletion("typedClassConstants", "    public const ^WITHOUT_TYPE = 1; // trait");
+    }
+
+    public void testTypedClassConstants_Trait02() throws Exception {
+        checkCompletion("typedClassConstants", "    public const ?^A NULLABLE = null; // trait");
+    }
+
+    public void testTypedClassConstants_Trait03() throws Exception {
+        checkCompletion("typedClassConstants", "    public const ?in^t NULLABLE2 = 1; // trait");
+    }
+
+    public void testTypedClassConstants_Trait04a() throws Exception {
+        checkCompletion("typedClassConstants", "    private const ^A|B UNION = A; // trait");
+    }
+
+    public void testTypedClassConstants_Trait04b() throws Exception {
+        checkCompletion("typedClassConstants", "    private const A|^B UNION = A; // trait");
+    }
+
+    public void testTypedClassConstants_Trait04c() throws Exception {
+        checkCompletion("typedClassConstants", "    private const A|B^ UNION = A; // trait");
+    }
+
+    public void testTypedClassConstants_Trait05a() throws Exception {
+        checkCompletion("typedClassConstants", "    protected const ^A&B INTERSECTION = B; // trait");
+    }
+
+    public void testTypedClassConstants_Trait05b() throws Exception {
+        checkCompletion("typedClassConstants", "    protected const A^&B INTERSECTION = B; // trait");
+    }
+
+    public void testTypedClassConstants_Trait05c() throws Exception {
+        checkCompletion("typedClassConstants", "    protected const A&^B INTERSECTION = B; // trait");
+    }
+
+    public void testTypedClassConstants_Trait06a() throws Exception {
+        checkCompletion("typedClassConstants", "    public const (^A&B)|C DNF = C; // trait");
+    }
+
+    public void testTypedClassConstants_Trait06b() throws Exception {
+        checkCompletion("typedClassConstants", "    public const (A^&B)|C DNF = C; // trait");
+    }
+
+    public void testTypedClassConstants_Trait06c() throws Exception {
+        checkCompletion("typedClassConstants", "    public const (A&^B)|C DNF = C; // trait");
+    }
+
+    public void testTypedClassConstants_Trait06d() throws Exception {
+        checkCompletion("typedClassConstants", "    public const (A&B^)|C DNF = C; // trait");
+    }
+
+    public void testTypedClassConstants_Trait06e() throws Exception {
+        checkCompletion("typedClassConstants", "    public const (A&B)|^C DNF = C; // trait");
+    }
+
+    public void testTypedClassConstants_Trait06f() throws Exception {
+        checkCompletion("typedClassConstants", "    public const (A&B)|C^ DNF = C; // trait");
+    }
+
+    public void testTypedClassConstants_Trait07() throws Exception {
+        checkCompletion("typedClassConstants", "    protected const A&B INTERSECTION = ^B; // trait");
+    }
+
+    public void testTypedClassConstants_Enum01() throws Exception {
+        checkCompletion("typedClassConstants", "    public const ^WITHOUT_TYPE = 1; // enum");
+    }
+
+    public void testTypedClassConstants_Enum02() throws Exception {
+        checkCompletion("typedClassConstants", "    public const ?^A NULLABLE = null; // enum");
+    }
+
+    public void testTypedClassConstants_Enum03() throws Exception {
+        checkCompletion("typedClassConstants", "    public const ?in^t NULLABLE2 = 1; // enum");
+    }
+
+    public void testTypedClassConstants_Enum04a() throws Exception {
+        checkCompletion("typedClassConstants", "    private const ^A|B UNION = A; // enum");
+    }
+
+    public void testTypedClassConstants_Enum04b() throws Exception {
+        checkCompletion("typedClassConstants", "    private const A|^B UNION = A; // enum");
+    }
+
+    public void testTypedClassConstants_Enum04c() throws Exception {
+        checkCompletion("typedClassConstants", "    private const A|B^ UNION = A; // enum");
+    }
+
+    public void testTypedClassConstants_Enum05a() throws Exception {
+        checkCompletion("typedClassConstants", "    protected const ^A&B INTERSECTION = B; // enum");
+    }
+
+    public void testTypedClassConstants_Enum05b() throws Exception {
+        checkCompletion("typedClassConstants", "    protected const A^&B INTERSECTION = B; // enum");
+    }
+
+    public void testTypedClassConstants_Enum05c() throws Exception {
+        checkCompletion("typedClassConstants", "    protected const A&^B INTERSECTION = B; // enum");
+    }
+
+    public void testTypedClassConstants_Enum06a() throws Exception {
+        checkCompletion("typedClassConstants", "    public const (^A&B)|C DNF = C; // enum");
+    }
+
+    public void testTypedClassConstants_Enum06b() throws Exception {
+        checkCompletion("typedClassConstants", "    public const (A^&B)|C DNF = C; // enum");
+    }
+
+    public void testTypedClassConstants_Enum06c() throws Exception {
+        checkCompletion("typedClassConstants", "    public const (A&^B)|C DNF = C; // enum");
+    }
+
+    public void testTypedClassConstants_Enum06d() throws Exception {
+        checkCompletion("typedClassConstants", "    public const (A&B^)|C DNF = C; // enum");
+    }
+
+    public void testTypedClassConstants_Enum06e() throws Exception {
+        checkCompletion("typedClassConstants", "    public const (A&B)|^C DNF = C; // enum");
+    }
+
+    public void testTypedClassConstants_Enum06f() throws Exception {
+        checkCompletion("typedClassConstants", "    public const (A&B)|C^ DNF = C; // enum");
+    }
+
+    public void testTypedClassConstants_Enum07() throws Exception {
+        checkCompletion("typedClassConstants", "    public const (A&B)|C DNF = ^C; // enum");
+    }
+
+    public void testTypedClassConstantsTypingClass01() throws Exception {
+        checkCompletion("typedClassConstantsTypingClass01", "    public const ^");
+    }
+
+    public void testTypedClassConstantsTypingClass02() throws Exception {
+        checkCompletion("typedClassConstantsTypingClass02", "    public const string^");
+    }
+
+    public void testTypedClassConstantsTypingClass03() throws Exception {
+        checkCompletion("typedClassConstantsTypingClass03", "    public const string|^");
+    }
+
+    public void testTypedClassConstantsTypingClass04() throws Exception {
+        checkCompletion("typedClassConstantsTypingClass04", "    public const A&^");
+    }
+
+    public void testTypedClassConstantsTypingClass05() throws Exception {
+        checkCompletion("typedClassConstantsTypingClass05", "    public const A&B&Te^");
+    }
+
+    public void testTypedClassConstantsTypingClass06() throws Exception {
+        checkCompletion("typedClassConstantsTypingClass06", "    public const (A&^)");
+    }
+
+    public void testTypedClassConstantsTypingClass07() throws Exception {
+        checkCompletion("typedClassConstantsTypingClass07", "    public const (A&B)|^");
+    }
+
+    public void testTypedClassConstantsTypingClass08() throws Exception {
+        checkCompletion("typedClassConstantsTypingClass08", "    public const (A&B)|(^)");
+    }
+
+    public void testTypedClassConstantsTypingClass09() throws Exception {
+        checkCompletion("typedClassConstantsTypingClass09", "    public const (A&B)|(A&C) C^");
+    }
+
+    public void testTypedClassConstantsTypingClass10() throws Exception {
+        checkCompletion("typedClassConstantsTypingClass10", "    public const (A&B)|(A&C) CONST_NAME = ^");
+    }
+
+    public void testTypedClassConstantsTypingInterface01() throws Exception {
+        checkCompletion("typedClassConstantsTypingInterface01", "    public const ^");
+    }
+
+    public void testTypedClassConstantsTypingInterface02() throws Exception {
+        checkCompletion("typedClassConstantsTypingInterface02", "    public const string^");
+    }
+
+    public void testTypedClassConstantsTypingInterface03() throws Exception {
+        checkCompletion("typedClassConstantsTypingInterface03", "    public const string|^");
+    }
+
+    public void testTypedClassConstantsTypingInterface04() throws Exception {
+        checkCompletion("typedClassConstantsTypingInterface04", "    public const A&^");
+    }
+
+    public void testTypedClassConstantsTypingInterface05() throws Exception {
+        checkCompletion("typedClassConstantsTypingInterface05", "    public const A&B&C^");
+    }
+
+    public void testTypedClassConstantsTypingInterface06() throws Exception {
+        checkCompletion("typedClassConstantsTypingInterface06", "    public const (A&^)");
+    }
+
+    public void testTypedClassConstantsTypingInterface07() throws Exception {
+        checkCompletion("typedClassConstantsTypingInterface07", "    public const (A&B)|^");
+    }
+
+    public void testTypedClassConstantsTypingInterface08() throws Exception {
+        checkCompletion("typedClassConstantsTypingInterface08", "    public const (A&B)|(^)");
+    }
+
+    public void testTypedClassConstantsTypingInterface09() throws Exception {
+        checkCompletion("typedClassConstantsTypingInterface09", "    public const (A&B)|(A&C) C^");
+    }
+
+    public void testTypedClassConstantsTypingInterface10() throws Exception {
+        checkCompletion("typedClassConstantsTypingInterface10", "    public const (A&B)|(A&C) CONST_NAME = ^");
+    }
+
 }

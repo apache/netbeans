c8683b987343f67ef040d66d9b3a5055bc573ed3
Svata Dedic <svatopluk.dedic@oracle.com>
2023-12-13 22:43:40 +0100

    Prototype of dependencies.find command

--- java/java.lsp.server/nbcode/integration/src/org/netbeans/modules/nbcode/integration/ExtraGsonSetup.java
+++ java/java.lsp.server/nbcode/integration/src/org/netbeans/modules/nbcode/integration/ExtraGsonSetup.java
@@ -21,11 +21,21 @@ package org.netbeans.modules.nbcode.integration;
 import com.google.gson.ExclusionStrategy;
 import com.google.gson.FieldAttributes;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import org.netbeans.modules.java.lsp.server.LspGsonSetup;
 import org.netbeans.modules.project.dependency.ArtifactSpec;
+import org.netbeans.modules.project.dependency.Dependency;
+import org.netbeans.modules.project.dependency.Scope;
 import org.openide.util.lookup.ServiceProvider;
 
 /**
@@ -40,6 +50,11 @@ public class ExtraGsonSetup implements LspGsonSetup{
             "data" // NOI18N
     ));
 
+    private static final Set<String> DEPENDENCY_BLOCK_FIELDS = new HashSet<>(Arrays.asList(
+            "parent", // NOI18N
+            "data" // NOI18N
+    ));
+
     @Override
     public void configureBuilder(GsonBuilder b) {
         b.addSerializationExclusionStrategy(new ExclusionStrategy() {
@@ -50,7 +65,9 @@ public class ExtraGsonSetup implements LspGsonSetup{
                 if (fa.getDeclaringClass() == ArtifactSpec.class) {
                     return ARTIFACT_BLOCK_FIELDS.contains(fa.getName());
                 } else if (Throwable.class.isAssignableFrom(fa.getDeclaredClass())) {
-
+                    return DEPENDENCY_BLOCK_FIELDS.contains(fa.getName());
+                } else if (fa.getDeclaringClass() == Dependency.class) {
+                    
                 }
                 return false;
             }
@@ -60,6 +77,36 @@ public class ExtraGsonSetup implements LspGsonSetup{
                 return false;
             }
         });
+        b.registerTypeAdapter(ArtifactSpec.class, new ArtifactDeserializer());
+        b.registerTypeAdapter(Scope.class, new ScopeSerializer());
     }
     
+    class ScopeSerializer implements JsonSerializer<Scope> {
+
+        @Override
+        public JsonElement serialize(Scope t, Type type, JsonSerializationContext jsc) {
+            return jsc.serialize(t.name());
+        }
+    }
+    
+    
+    class ArtifactDeserializer implements JsonDeserializer<ArtifactSpec> {
+
+        @Override
+        public ArtifactSpec deserialize(JsonElement je, Type type, JsonDeserializationContext jdc) throws JsonParseException {
+            JsonObject obj = je.getAsJsonObject();
+            String g = obj.has("groupId") ? obj.getAsJsonPrimitive("groupId").getAsString() : null;
+            String a = obj.has("artifactId") ? obj.getAsJsonPrimitive("artifactId").getAsString() : null;
+            String v = obj.has("versionSpec") ? obj.getAsJsonPrimitive("versionSpec").getAsString() : null;
+            String c = obj.has("classifier") ? obj.getAsJsonPrimitive("classifier").getAsString() : null;
+            String t = obj.has("type") ? obj.getAsJsonPrimitive("type").getAsString() : null;
+            
+            
+            ArtifactSpec.Builder b = ArtifactSpec.builder(g, a, v, null).classifier(c).type(t);
+            if (v != null && v.contains("-SNAPSHOT")) {
+                b.versionKind(ArtifactSpec.VersionKind.SNAPSHOT);
+            }
+            return b.build();
+        }
+    }
 }

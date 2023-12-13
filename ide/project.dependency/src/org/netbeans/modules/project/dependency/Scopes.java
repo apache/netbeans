8c1c42b82efc21dae3457d9e2f52bb1b220dc4e0
Svata Dedic <svatopluk.dedic@oracle.com>
2023-12-13 17:08:51 +0100

    Scope redefinition, private API break.

--- ide/project.dependency/src/org/netbeans/modules/project/dependency/Scopes.java
+++ ide/project.dependency/src/org/netbeans/modules/project/dependency/Scopes.java
@@ -18,11 +18,6 @@
  */
 package org.netbeans.modules.project.dependency;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  *
  * @author sdedic
@@ -31,82 +26,57 @@ public final class Scopes {
     /**
      * Build process dependencies. Annotation processors, buildtime tools, code generators
      */
-    public static final Scope PROCESS = new DefaultScope("compileProcessing", Collections.emptySet(), Collections.emptySet());
+    public static final Scope PROCESS = new Scope("compileProcessing");
 
     /**
      * External dependencies, not distributed with the application, but provided by the environment (= provided dependencies in Maven)
      */
-    public static final Scope EXTERNAL = new DefaultScope("external", Collections.emptySet(), Collections.emptySet());
+    public static final Scope EXTERNAL = new Scope("external");
     
     /**
-     * Compile dependencies. Resources used by build tools to build the application. Includes 
-     * {@link #PROCESS} but does not export it further.
+     * Compile API dependencies. Optional, if the build system supports it. Otherwise should be equal to 
+     * {@link #COMPILE}. Gradle makes a difference between API and implementation. 
      */
-    public static final Scope COMPILE = new DefaultScope("compilation", 
-            Collections.singleton(PROCESS), Collections.singleton(PROCESS));
+    public static final Scope API = new Scope("api");
     
     /**
-     * Runtime dependencies. Includes compile dependencies.
+     * Compile dependencies. Resources used by build tools to build the application. 
      */
-    public static final Scope RUNTIME = new DefaultScope("runtime", Collections.singleton(COMPILE), Collections.emptySet());
+    public static final Scope COMPILE = new Scope("compilation");
     
     /**
-     * Test compile dependencies.
+     * Runtime dependencies. Includes compile dependencies, but not necessarily all of them.
      */
-    public static final Scope TEST_COMPILE = new DefaultScope("testCompile", 
-            new HashSet<>(Arrays.asList(PROCESS, COMPILE)), Collections.emptySet());
+    public static final Scope RUNTIME = new Scope("runtime");
     
     /**
-     * Test compile dependencies.
+     * Test compile dependencies. Optional, if the build system supports it.
      */
-    public static final Scope TEST_RUNTIME = new DefaultScope("testRuntime", 
-            new HashSet<>(Arrays.asList(TEST_COMPILE)), Collections.emptySet());
+    public static final Scope TEST_COMPILE = new Scope("testCompile");
     
     /**
-     * Test dependencies.
+     * Test runtime dependencies. Optional, if the build system supports it.
      */
-    public static final Scope TEST = new DefaultScope("test", 
-            new HashSet<>(Arrays.asList(PROCESS, COMPILE, RUNTIME)), Collections.emptySet()).imply(TEST_RUNTIME, TEST_COMPILE);
+    public static final Scope TEST_RUNTIME = new Scope("testRuntime");
     
     /**
-     * Included resources.
-    public static final Scope INCLUDED = new DefaultScope("included", Collections.emptySet(), Collections.emptySet());
+     * Generic test dependencies. 
      */
+    public static final Scope TEST = new Scope("test");
     
-    static final class DefaultScope extends Scope {
-        private final Set<Scope> includes;
-        private final Set<Scope> stops;
-        private Set<Scope> implies;
+    /**
+     * Dependencies directly declared by the project definition. Can be combined with other types to select just specific
+     * dependencies. Note that dependencies obtained using this modifier may be incomplete or version-unresolved, if they appear so
+     * in the build file. 
+     * <p>
+     * Note that it is not possible to add dependencies with this scope (exception will be thrown), it only serves as marker. Also no dependency
+     * will not be marked with this scope, all dependencies retain the scope they are declared for in the project's metadata.
+     */
+    public static final Scope DECLARED = new Scope("*declared");
 
-        public DefaultScope(String name, Set<Scope> includes, Set<Scope> stops) {
-            super(name);
-            this.includes = includes;
-            this.stops = stops;
-        }
-
-        @Override
-        public boolean includes(Scope s) {
-            return s == this || includes.contains(s);
-        }
-
-        @Override
-        public boolean exports(Scope s) {
-            return s == this || (!stops.contains(s) && includes(s));
-        }
-
-        @Override
-        public String toString() {
-            return name();
-        }
-
-        @Override
-        public boolean implies(Scope s) {
-            return implies != null && implies.contains(s);
-        }
-        
-        public DefaultScope imply(Scope... scopes) {
-            this.implies = new HashSet<>(Arrays.asList(scopes));
-            return this;
-        }
-    }
+    /**
+     * Represents a plugin dependency. Plugin dependency extends applies a plugin to a build. PLUGIN dependency artifacts
+     * specify names and versions of those plugins. 
+     */
+    public static final Scope PLUGIN = new org.netbeans.modules.project.dependency.Scope("*plugin");
 }

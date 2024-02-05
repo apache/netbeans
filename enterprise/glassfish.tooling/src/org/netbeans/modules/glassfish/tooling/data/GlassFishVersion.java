281694f68ab18bd8273b2d8536b099239c2a1d3d
José Contreras <pepness@apache.org>
2024-02-05 17:49:11 -0600

    Add support for GlassFish 7.0.12

--- enterprise/glassfish.tooling/src/org/netbeans/modules/glassfish/tooling/data/GlassFishVersion.java
+++ enterprise/glassfish.tooling/src/org/netbeans/modules/glassfish/tooling/data/GlassFishVersion.java
@@ -120,7 +120,9 @@ public enum GlassFishVersion {
     /** GlassFish 7.0.10 */
     GF_7_0_10       ((short) 7, (short) 0, (short) 10, (short) 0, GlassFishVersion.GF_7_0_10_STR),
     /** GlassFish 7.0.11 */
-    GF_7_0_11       ((short) 7, (short) 0, (short) 11, (short) 0, GlassFishVersion.GF_7_0_11_STR);
+    GF_7_0_11       ((short) 7, (short) 0, (short) 11, (short) 0, GlassFishVersion.GF_7_0_11_STR),
+    /** GlassFish 7.0.12 */
+    GF_7_0_12       ((short) 7, (short) 0, (short) 12, (short) 0, GlassFishVersion.GF_7_0_12_STR);
     ////////////////////////////////////////////////////////////////////////////
     // Class attributes                                                       //
     ////////////////////////////////////////////////////////////////////////////
@@ -331,6 +333,11 @@ public enum GlassFishVersion {
     /** Additional {@code String} representations of GF_7_0_11 value. */
     static final String GF_7_0_11_STR_NEXT[] = {"7.0.11", "7.0.11.0"};
 
+    /** A {@code String} representation of GF_7_0_12 value. */
+    static final String GF_7_0_12_STR = "7.0.12";
+    /** Additional {@code String} representations of GF_7_0_12 value. */
+    static final String GF_7_0_12_STR_NEXT[] = {"7.0.12", "7.0.12.0"};
+
     /**
      * Stored <code>String</code> values for backward <code>String</code>
      * conversion.
@@ -379,6 +386,7 @@ public enum GlassFishVersion {
         initStringValuesMapFromArray(GF_7_0_9, GF_7_0_9_STR_NEXT);
         initStringValuesMapFromArray(GF_7_0_10, GF_7_0_10_STR_NEXT);
         initStringValuesMapFromArray(GF_7_0_11, GF_7_0_11_STR_NEXT);
+        initStringValuesMapFromArray(GF_7_0_12, GF_7_0_12_STR_NEXT);
     }
 
     ////////////////////////////////////////////////////////////////////////////

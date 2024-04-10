5f041abf9ddc2cf9929321df025a2eebce59c9ab
Matthias Bläsing <mblaesing@doppel-helix.eu>
2024-04-10 19:03:42 +0200

    Reduce system property modifications for GraalVM

--- ide/libs.graalsdk/src/org/netbeans/libs/graalsdk/impl/Installer.java
+++ ide/libs.graalsdk/src/org/netbeans/libs/graalsdk/impl/Installer.java
@@ -8,10 +8,17 @@ public class Installer extends ModuleInstall {
     @Override
     public void validate() {
         super.validate();
-        // If truffle runtimes are executed directly they try to load another
-        // copy of the native integration library. This fails. The fallback
-        // runtime can run independently of this.
-        System.setProperty( "polyglot.engine.WarnInterpreterOnly", "false" ); //NOI18N
-        System.setProperty( "truffle.UseFallbackRuntime", "true" ); //NOI18N
+        // Truffle runtime is very vocal if it is run in interpreter only mode.
+        // That mode is the case in "normal" JDKs, which are the NetBeans
+        // baseline - the warnings should be silenced accordingly
+        if (!System.getProperties().contains("polyglot.engine.WarnInterpreterOnly")) { //NOI18N
+            System.setProperty("polyglot.engine.WarnInterpreterOnly", "false"); //NOI18N
+        }
+        // The default Truffle runtime uses a native library. That library fails
+        // hard when it shall be loaded by multiple classloaders. So for now
+        // use the fallback runtime
+        if (!System.getProperties().contains("truffle.UseFallbackRuntime")) { //NOI18N
+            System.setProperty( "truffle.UseFallbackRuntime", "true" ); //NOI18N
+        }
     }
 }

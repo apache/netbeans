b8d43a32cc67ee8f46b243bce25281ebeb1a8240
Michael Bien <mbien42@gmail.com>
2024-10-03 00:02:09 +0200

    Switch from memory fs to plain old files.
     - testing showed that using a real filesystem (or not using mem fs)
       resolves the sporadic failures entirely. Unfortunately it is
       not clear what the cause for the failures is. (see PR discussion)
     - move test source files into folder matching the declared package

--- java/java.source.base/test/unit/src/org/netbeans/modules/java/source/parsing/PartialReparseTest.java
+++ java/java.source.base/test/unit/src/org/netbeans/modules/java/source/parsing/PartialReparseTest.java
@@ -71,6 +71,7 @@ public class PartialReparseTest extends NbTestCase {
     @Override
     protected void setUp() throws Exception {
         super.setUp();
+        clearWorkDir();
         SourceUtilsTestUtil.prepareTest(new String[0], new Object[0]);
         // ensure JavaKit is present, so that NbEditorDocument is eventually created.
         // it handles PositionRefs differently than PlainDocument/PlainEditorKit.
@@ -385,8 +386,8 @@ public class PartialReparseTest extends NbTestCase {
 
     private void doRunTest(String code, String inject, Consumer<CompilationInfo> callback) throws Exception {
 
-        FileObject srcDir = FileUtil.createMemoryFileSystem().getRoot();
-        FileObject src = srcDir.createData("Test.java");
+        FileObject srcDir = FileUtil.createFolder(getWorkDir());
+        FileObject src = srcDir.createFolder("test").createData("Test.java");
 
         // parse original source
         String codeInput = code.replace("^", "");
@@ -437,8 +438,8 @@ public class PartialReparseTest extends NbTestCase {
 
     private void doVerifyFullReparse(String code, String inject) throws Exception {
 
-        FileObject srcDir = FileUtil.createMemoryFileSystem().getRoot();
-        FileObject src = srcDir.createData("Test.java");
+        FileObject srcDir = FileUtil.createFolder(getWorkDir());
+        FileObject src = srcDir.createFolder("test").createData("Test.java");
 
         // parse original source
         String codeInput = code.replace("^", "");

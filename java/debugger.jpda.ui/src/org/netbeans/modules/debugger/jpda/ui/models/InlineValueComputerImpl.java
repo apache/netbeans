75516923188bd37f7a7c42c3b11c38609fbac5eb
Jan Lahoda <jan.lahoda@oracle.com>
2025-04-01 08:03:55 +0200

    Adding a setting to switch inline values on/off.

--- java/debugger.jpda.ui/src/org/netbeans/modules/debugger/jpda/ui/models/InlineValueComputerImpl.java
+++ java/debugger.jpda.ui/src/org/netbeans/modules/debugger/jpda/ui/models/InlineValueComputerImpl.java
@@ -36,6 +36,9 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.prefs.PreferenceChangeEvent;
+import java.util.prefs.PreferenceChangeListener;
+import java.util.prefs.Preferences;
 import java.util.stream.Collectors;
 import javax.swing.text.AttributeSet;
 import javax.swing.text.Document;
@@ -48,6 +51,7 @@ import org.netbeans.api.debugger.jpda.JPDAClassType;
 import org.netbeans.api.debugger.jpda.JPDADebugger;
 import org.netbeans.api.debugger.jpda.ObjectVariable;
 import org.netbeans.api.debugger.jpda.Variable;
+import org.netbeans.api.editor.mimelookup.MimeLookup;
 import org.netbeans.api.editor.mimelookup.MimeRegistration;
 import org.netbeans.api.editor.settings.AttributesUtilities;
 import org.netbeans.api.java.source.CancellableTask;
@@ -68,6 +72,7 @@ import org.netbeans.modules.debugger.jpda.ui.values.ComputeInlineValues.InlineVa
 import org.netbeans.modules.parsing.spi.TaskIndexingMode;
 import org.netbeans.spi.debugger.ContextProvider;
 import org.netbeans.spi.debugger.DebuggerServiceRegistration;
+import org.netbeans.spi.debugger.ui.Constants;
 import org.netbeans.spi.editor.highlighting.HighlightsLayer;
 import org.netbeans.spi.editor.highlighting.HighlightsLayerFactory;
 import org.netbeans.spi.editor.highlighting.HighlightsSequence;
@@ -79,22 +84,26 @@ import org.openide.filesystems.URLMapper;
 import org.openide.util.Exceptions;
 import org.openide.util.Lookup;
 import org.openide.util.RequestProcessor;
+import org.openide.util.WeakListeners;
 import org.openide.util.lookup.ServiceProvider;
 import org.openide.util.lookup.ServiceProviders;
 
 
 @DebuggerServiceRegistration(path="netbeans-JPDASession/inlineValue", types=InlineValueComputer.class)
-public class InlineValueComputerImpl implements InlineValueComputer, PropertyChangeListener {
+public class InlineValueComputerImpl implements InlineValueComputer, PreferenceChangeListener, PropertyChangeListener {
 
     private static final Logger LOG = Logger.getLogger(InlineValueComputerImpl.class.getName());
     private static final RequestProcessor EVALUATOR = new RequestProcessor(InlineValueComputerImpl.class.getName(), 1, false, false);
     private static final String JAVA_STRATUM = "Java"; //XXX: this is probably already defined somewhere
     private final JPDADebuggerImpl debugger;
+    private final Preferences prefs;
     private TaskDescription currentTask;
 
     public InlineValueComputerImpl(ContextProvider contextProvider) {
         debugger = (JPDADebuggerImpl) contextProvider.lookupFirst(null, JPDADebugger.class);
         debugger.addPropertyChangeListener(this);
+        prefs = MimeLookup.getLookup("text/x-java").lookup(Preferences.class);
+        prefs.addPreferenceChangeListener(WeakListeners.create(PreferenceChangeListener.class, this, prefs));
     }
 
     @Override
@@ -105,108 +114,118 @@ public class InlineValueComputerImpl implements InlineValueComputer, PropertyCha
         }
 
         if (JPDADebugger.PROP_CURRENT_CALL_STACK_FRAME.equals(evt.getPropertyName())) {
-            CallStackFrame frame = debugger.getCurrentCallStackFrame();
+            refreshVariables();
+        }
+    }
 
-            FileObject frameFile = null;
-            int frameLineNumber = -1;
-            Document frameDocument = null;
+    @Override
+    public void preferenceChange(PreferenceChangeEvent evt) {
+        refreshVariables();
+    }
 
-            if (frame != null && !frame.isObsolete() &&
-                frame.getThread().isSuspended() &&
-                JAVA_STRATUM.equals(frame.getDefaultStratum())) {
+    private void refreshVariables() {
+        CallStackFrame frame = debugger.getCurrentCallStackFrame();
+
+        FileObject frameFile = null;
+        int frameLineNumber = -1;
+        Document frameDocument = null;
+
+        if (prefs.getBoolean(Constants.KEY_INLINE_VALUES, Constants.DEF_INLINE_VALUES) &&
+            frame != null && !frame.isObsolete() &&
+            frame.getThread().isSuspended() &&
+            JAVA_STRATUM.equals(frame.getDefaultStratum())) {
+            try {
+                String url = debugger.getEngineContext().getURL(frame, JAVA_STRATUM);
+                frameFile = url != null ? URLMapper.findFileObject(URI.create(url).toURL()) : null;
+                if (frameFile != null) {
+                    frameLineNumber = frame.getLineNumber(JAVA_STRATUM);
+                    EditorCookie ec = frameFile.getLookup().lookup(EditorCookie.class);
+                    frameDocument = ec != null ? ec.getDocument() : null;
+                }
+            } catch (InternalExceptionWrapper | InvalidStackFrameExceptionWrapper | ObjectCollectedExceptionWrapper | VMDisconnectedExceptionWrapper | MalformedURLException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+        }
+
+        TaskDescription newTask;
+
+        if (frameFile != null && frameDocument != null) {
+            newTask = new TaskDescription(frameFile, frameLineNumber, frameDocument);
+        } else {
+            newTask = null;
+        }
+
+        if (setNewTask(newTask)) {
+            return;
+        }
+
+        if (newTask != null) {
+            //TODO: cancel any already running computation if the configuration is different:
+            CountDownLatch computationDone = new CountDownLatch(1);
+
+            newTask.addCancelCallback(computationDone::countDown);
+
+            AtomicReference<Collection<InlineVariable>> values = new AtomicReference<>();
+
+            EVALUATOR.post(() -> {
+                OffsetsBag runningBag = new OffsetsBag(newTask.frameDocument);
+
+                Lookup.getDefault().lookup(ComputeInlineVariablesFactory.class).set(newTask.frameFile, newTask.frameLineNumber, variables -> {
+                    values.set(variables);
+                    computationDone.countDown();
+                });
+
                 try {
-                    String url = debugger.getEngineContext().getURL(frame, JAVA_STRATUM);
-                    frameFile = url != null ? URLMapper.findFileObject(URI.create(url).toURL()) : null;
-                    if (frameFile != null) {
-                        frameLineNumber = frame.getLineNumber(JAVA_STRATUM);
-                        EditorCookie ec = frameFile.getLookup().lookup(EditorCookie.class);
-                        frameDocument = ec != null ? ec.getDocument() : null;
-                    }
-                } catch (InternalExceptionWrapper | InvalidStackFrameExceptionWrapper | ObjectCollectedExceptionWrapper | VMDisconnectedExceptionWrapper | MalformedURLException ex) {
+                    computationDone.await();
+                } catch (InterruptedException ex) {
                     Exceptions.printStackTrace(ex);
                 }
-            }
 
-            TaskDescription newTask;
+                if (newTask.isCancelled()) {
+                    return ;
+                }
 
-            if (frameFile != null && frameDocument != null) {
-                newTask = new TaskDescription(frameFile, frameLineNumber, frameDocument);
-            } else {
-                newTask = null;
-            }
+                Collection<InlineVariable> variables = values.get();
 
-            if (setNewTask(newTask)) {
-                return;
-            }
+                if (values == null) {
+                    return ;
+                }
 
-            if (newTask != null) {
-                //TODO: cancel any already running computation if the configuration is different:
-                CountDownLatch computationDone = new CountDownLatch(1);
+                Map<String, Variable> expression2Value = new HashMap<>();
+                Map<Integer, Map<String, String>> line2Values = new HashMap<>();
 
-                newTask.addCancelCallback(computationDone::countDown);
-
-                AtomicReference<Collection<InlineVariable>> values = new AtomicReference<>();
-
-                EVALUATOR.post(() -> {
-                    OffsetsBag runningBag = new OffsetsBag(newTask.frameDocument);
-
-                    Lookup.getDefault().lookup(ComputeInlineVariablesFactory.class).set(newTask.frameFile, newTask.frameLineNumber, variables -> {
-                        values.set(variables);
-                        computationDone.countDown();
-                    });
-
-                    try {
-                        computationDone.await();
-                    } catch (InterruptedException ex) {
-                        Exceptions.printStackTrace(ex);
-                    }
-
+                for (InlineVariable v : variables) {
                     if (newTask.isCancelled()) {
                         return ;
                     }
 
-                    Collection<InlineVariable> variables = values.get();
-
-                    if (values == null) {
-                        return ;
-                    }
-
-                    Map<String, Variable> expression2Value = new HashMap<>();
-                    Map<Integer, Map<String, String>> line2Values = new HashMap<>();
-
-                    for (InlineVariable v : variables) {
-                        if (newTask.isCancelled()) {
-                            return ;
+                    Variable value = expression2Value.computeIfAbsent(v.expression, expr -> {
+                        try {
+                            return debugger.evaluate(expr);
+                        } catch (InvalidExpressionException ex) {
+                            //the variable may not exist
+                            LOG.log(Level.FINE, null, ex);
+                            return null;
                         }
-
-                        Variable value = expression2Value.computeIfAbsent(v.expression, expr -> {
-                            try {
-                                return debugger.evaluate(expr);
-                            } catch (InvalidExpressionException ex) {
-                                //the variable may not exist
-                                LOG.log(Level.FINE, null, ex);
-                                return null;
-                            }
-                        });
-                        if (value != null) {
-                            String valueText;
-                            if (value instanceof ObjectVariable ov) {
-                                valueText = toValue(ov).replace("\n", "\\n");
-                            } else {
-                                valueText = value.getValue();
-                            }
-                            line2Values.computeIfAbsent(v.lineEnd, __ -> new LinkedHashMap<>())
-                                       .putIfAbsent(v.expression, v.expression + " = " + valueText);
-                            String mergedValues = line2Values.get(v.lineEnd).values().stream().collect(Collectors.joining(", ", "  ", ""));
-                            AttributeSet attrs = AttributesUtilities.createImmutable("virtual-text-prepend", mergedValues);
-
-                            runningBag.addHighlight(v.lineEnd, v.lineEnd + 1, attrs);
-
-                            setHighlights(newTask, runningBag);
+                    });
+                    if (value != null) {
+                        String valueText;
+                        if (value instanceof ObjectVariable ov) {
+                            valueText = toValue(ov).replace("\n", "\\n");
+                        } else {
+                            valueText = value.getValue();
                         }
+                        line2Values.computeIfAbsent(v.lineEnd, __ -> new LinkedHashMap<>())
+                                   .putIfAbsent(v.expression, v.expression + " = " + valueText);
+                        String mergedValues = line2Values.get(v.lineEnd).values().stream().collect(Collectors.joining(", ", "  ", ""));
+                        AttributeSet attrs = AttributesUtilities.createImmutable("virtual-text-prepend", mergedValues);
+
+                        runningBag.addHighlight(v.lineEnd, v.lineEnd + 1, attrs);
+
+                        setHighlights(newTask, runningBag);
                     }
-                });
-            }
+                }
+            });
         }
     }
 

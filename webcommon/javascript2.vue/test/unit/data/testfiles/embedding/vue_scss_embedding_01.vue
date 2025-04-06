c41255daf4a0803789976b0f1e411b5048b4158b
Bogdan Haidu <haidubogdan@gmail.com>
2025-04-06 12:58:15 +0300

    include parametric embedding support for javascript, pug | jade, less, sass remove uncessary antlr lexer flags

---
+++ webcommon/javascript2.vue/test/unit/data/testfiles/embedding/vue_scss_embedding_01.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+const greeting = ref('Hello World!')
+</script>
+
+<template>
+  <p class="greeting">{{ greeting }}</p>
+  <p class="footer"></p>
+</template>
+
+<style lang="scss">
+    $font-stack: Helvetica, sans-serif;
+    $primary-color: #333;
+
+    p {
+      font: 100% $font-stack;
+      color: $primary-color;
+    }
+    @mixin theme($theme: DarkGray) {
+      background: $theme;
+      box-shadow: 0 0 1px rgba($theme, .25);
+      color: #fff;
+    }
+
+    .info {
+      @include theme;
+    }
+</style>

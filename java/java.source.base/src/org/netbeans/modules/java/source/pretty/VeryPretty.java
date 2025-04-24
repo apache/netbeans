70cffdb59d0b891b787f531e811cbd87694ce58d
Pieter van den Hombergh (homberghp) <p.vandenhombergh@fontys.nl>
2025-04-24 15:55:57 +0200

    minimal changes, no drive-by changes
    merged with master

--- java/java.source.base/src/org/netbeans/modules/java/source/pretty/VeryPretty.java
+++ java/java.source.base/src/org/netbeans/modules/java/source/pretty/VeryPretty.java
@@ -857,6 +857,7 @@ public final class VeryPretty extends JCTree.Visitor implements DocTreeVisitor<V
 
     @Override
     public void visitClassDef(JCClassDecl tree) {
+        System.err.println("VP visit clas/"+tree+"//class");
         JCClassDecl enclClassPrev = enclClass;
 	enclClass = tree;
 	toLeftMargin();

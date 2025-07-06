abb75d73c53a2c943c398bdd89fb1af4a4b7c63f
Michael Bien <mbien42@gmail.com>
2025-07-06 05:12:28 +0200

    Disable test case to avoid CCE in javac Lint.supresssionsFrom
    Test code breaks code scan and influences other tests.

--- java/java.editor/test/unit/data/org/netbeans/modules/java/editor/imports/data/Test232647.java
+++ java/java.editor/test/unit/data/org/netbeans/modules/java/editor/imports/data/Test232647.java
@@ -1,6 +1,7 @@
 package org.netbeans.modules.java.editor.imports.data;
 
-@SuppressWarnings(STR)
+// javac 25: class com.sun.tools.javac.code.Attribute$Error cannot be cast to class com.sun.tools.javac.code.Attribute$Constant (com.sun.tools.javac.code.Attribute$Error
+//@SuppressWarnings(STR)
 public class Test232647 {
     
     private static final String STR = "";

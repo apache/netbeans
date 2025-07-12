36f703e44a36316edd215ed55e90316bd0ecb15e
Christian Lenz <Christian.Lenz@gmx.net>
2025-07-12 16:07:35 +0200

    Add missinng logic for delete a local branch

--- ide/git/src/org/netbeans/modules/git/ui/menu/BranchMenu.java
+++ ide/git/src/org/netbeans/modules/git/ui/menu/BranchMenu.java
@@ -29,6 +29,7 @@ import org.netbeans.libs.git.GitBranch;
 import org.netbeans.modules.git.Annotator;
 import org.netbeans.modules.git.ui.branch.CherryPickAction;
 import org.netbeans.modules.git.ui.branch.CreateBranchAction;
+import org.netbeans.modules.git.ui.branch.DeleteBranchAction;
 import org.netbeans.modules.git.ui.branch.SetTrackingAction;
 import org.netbeans.modules.git.ui.checkout.AbstractCheckoutAction;
 import org.netbeans.modules.git.ui.checkout.SwitchBranchAction;
@@ -98,6 +99,7 @@ public final class BranchMenu extends DynamicMenu {
             Utils.setAcceleratorBindings(Annotator.ACTIONS_PATH_PREFIX, action);
             Actions.connect(item, action, false);
             menu.add(item);
+
             item = new JMenuItem();
             action = (Action) SystemAction.get(ManageTagsAction.class);
             Utils.setAcceleratorBindings(Annotator.ACTIONS_PATH_PREFIX, action);
@@ -119,6 +121,12 @@ public final class BranchMenu extends DynamicMenu {
             
             item = new JMenuItem();
             action = (Action) SystemAction.get(CherryPickAction.class);
+            Utils.setAcceleratorBindings(Annotator.ACTIONS_PATH_PREFIX, action);
+            Actions.connect(item, action, false);
+            menu.add(item);
+            
+            item = new JMenuItem();
+            action = (Action) SystemAction.get(DeleteBranchAction.class);
             Utils.setAcceleratorBindings(Annotator.ACTIONS_PATH_PREFIX, action);
             Actions.connect(item, action, false);
             menu.add(item);
@@ -164,6 +172,8 @@ public final class BranchMenu extends DynamicMenu {
             item = menu.add(SystemActionBridge.createAction(SystemAction.get(RebaseAction.class), NbBundle.getMessage(RebaseAction.class, "LBL_RebaseAction_PopupName"), lkp)); //NOI18N
             org.openide.awt.Mnemonics.setLocalizedText(item, item.getText());
             item = menu.add(SystemActionBridge.createAction(SystemAction.get(CherryPickAction.class), NbBundle.getMessage(CherryPickAction.class, "LBL_CherryPickAction_PopupName"), lkp)); //NOI18N
+            org.openide.awt.Mnemonics.setLocalizedText(item, item.getText());
+            item = menu.add(SystemActionBridge.createAction(SystemAction.get(DeleteBranchAction.class), NbBundle.getMessage(DeleteBranchAction.class, "LBL_DeleteBranchAction_PopupName"), lkp)); //NOI18N
             org.openide.awt.Mnemonics.setLocalizedText(item, item.getText());
         }        
         return menu;

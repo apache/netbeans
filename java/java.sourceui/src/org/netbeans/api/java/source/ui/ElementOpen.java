96bf942663122b31c1fa3d75a00572be15a30080
Shivam Madan <shivam.madan@oracle.com>
2025-09-23 12:23:50 +0530

    Set end position for implicit final class declaration tree part of compact source file compilation unit to end position of compilation unit tree.

--- java/java.sourceui/src/org/netbeans/api/java/source/ui/ElementOpen.java
+++ java/java.sourceui/src/org/netbeans/api/java/source/ui/ElementOpen.java
@@ -573,6 +573,10 @@ public final class ElementOpen {
         int[] span = null;
         switch(forTree.getKind()) {
             case CLASS:
+                if ((int) target[1] >= 0 && (int) target[2] == -1) {
+                // Compact Source file (JEP 512)  issue implicit class end position not found in code 
+                    target[2] = (int) info.getTrees().getSourcePositions().getEndPosition(cu, cu);
+                }
             case INTERFACE:
             case ENUM:
             case ANNOTATION_TYPE:

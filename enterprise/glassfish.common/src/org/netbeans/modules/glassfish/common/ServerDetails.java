90611c7f573b3ec733062e1c21c7b8954a78de2a
Davide Mainardi <davide@mainardisoluzioni.com>
2025-12-14 17:22:15 +0100

    Initial fixes before testing

--- enterprise/glassfish.common/src/org/netbeans/modules/glassfish/common/ServerDetails.java
+++ enterprise/glassfish.common/src/org/netbeans/modules/glassfish/common/ServerDetails.java
@@ -584,8 +584,8 @@ public enum ServerDetails {
     GLASSFISH_SERVER_8_0_0(NbBundle.getMessage(ServerDetails.class, "STR_800_SERVER_NAME", new Object[]{}), // NOI18N
         GlassfishInstanceProvider.JAKARTAEE11_DEPLOYER_FRAGMENT,
         GlassFishVersion.GF_8_0_0,
-        "https://repo.maven.apache.org/maven2/org/glassfish/main/distributions/glassfish/8.0.0-M12/glassfish-8.0.0-M14.zip", // NOI18N
-        "https://repo.maven.apache.org/maven2/org/glassfish/main/distributions/glassfish/8.0.0-M12/glassfish-8.0.0-M14.zip", // NOI18N
+        "https://repo.maven.apache.org/maven2/org/glassfish/main/distributions/glassfish/8.0.0-M14/glassfish-8.0.0-M14.zip", // NOI18N
+        "https://repo.maven.apache.org/maven2/org/glassfish/main/distributions/glassfish/8.0.0-M14/glassfish-8.0.0-M14.zip", // NOI18N
         "http://www.eclipse.org/legal/epl-2.0" //NOI18N
     );
     

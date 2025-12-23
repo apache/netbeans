b355a3edd08dba9520985f3268573115db823b67
Brad Walker <bwalker@musings.com>
2025-12-22 21:07:05 -0700

    cleanup: cleanup usage of getSecurityManager()
    desc: getSecurityManager() is deprecated and marked for imminent removal. In addition as of JDK 24, getSecurityManager() returns null.
    
    This change removes getSecurityManager() from various places that have minimal impact.

--- ide/spellchecker/src/org/netbeans/modules/spellchecker/options/CheckBoxRenderrer.java
+++ ide/spellchecker/src/org/netbeans/modules/spellchecker/options/CheckBoxRenderrer.java
@@ -49,19 +49,11 @@ public class CheckBoxRenderrer extends JCheckBox implements ListCellRenderer, Se
 
     private Border getNoFocusBorder () {
         Border border = UIManager.getBorder("List.cellNoFocusBorder");
-        if (System.getSecurityManager () != null) {
-            if (border != null) {
-                return border;
-            }
-            return SAFE_NO_FOCUS_BORDER;
-        } else {
-            if (border != null &&
-                (noFocusBorder == null ||
-                noFocusBorder == DEFAULT_NO_FOCUS_BORDER)) {
-                return border;
-            }
-            return noFocusBorder;
+        if (border != null &&
+            (noFocusBorder == null || noFocusBorder == DEFAULT_NO_FOCUS_BORDER)) {
+            return border;
         }
+        return noFocusBorder;
     }
 
     public Component getListCellRendererComponent (

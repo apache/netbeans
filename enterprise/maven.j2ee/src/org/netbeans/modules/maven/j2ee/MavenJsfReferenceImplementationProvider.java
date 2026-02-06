5fbee19b85380e1e67d04fd765cb616901313961
Nicola Isotta <nicola.isotta@gmail.com>
2026-02-06 12:05:46 +0100

    update versions

--- enterprise/maven.j2ee/src/org/netbeans/modules/maven/j2ee/MavenJsfReferenceImplementationProvider.java
+++ enterprise/maven.j2ee/src/org/netbeans/modules/maven/j2ee/MavenJsfReferenceImplementationProvider.java
@@ -55,8 +55,8 @@ public class MavenJsfReferenceImplementationProvider implements JsfReferenceImpl
         map.put(JsfVersion.JSF_2_2, "com.sun.faces:jsf-impl:2.2.20");
         map.put(JsfVersion.JSF_2_3, "org.glassfish:jakarta.faces:2.3.21");
         map.put(JsfVersion.JSF_3_0, "org.glassfish:jakarta.faces:3.0.5");
-        map.put(JsfVersion.JSF_4_0, "org.glassfish:jakarta.faces:4.0.13");
-        map.put(JsfVersion.JSF_4_1, "org.glassfish:jakarta.faces:4.1.5");
+        map.put(JsfVersion.JSF_4_0, "org.glassfish:jakarta.faces:4.0.14");
+        map.put(JsfVersion.JSF_4_1, "org.glassfish:jakarta.faces:4.1.6");
         JSF_VERSION_MAVEN_COORDINATES_MAPPING = Collections.unmodifiableMap(map);
     }
 

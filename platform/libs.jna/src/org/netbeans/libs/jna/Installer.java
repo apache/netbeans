a8edba0a6fa281cac211fb3eb749bdff50e7e3d3
Matthias Bläsing <mblaesing@doppel-helix.eu>
2026-06-23 21:48:08 +0200

    Make JNA (inkl. jna-platform) available as OSGI bundle instead of custom wrapping
    JNA provides OSGI header and this can (and does) lead to the problem
    where libraries depend on the bundle name of JNA.
    
    The triggering problem was org.eclipse.jgit.ssh.apache.agent, which has
    said bundle dependency.
    
    The NetBeans modules were retained to provide the following:
    
    - jna + jna-platform: provide module-auto-deps.xml so that modules
      depending on older versions of the modules will automatically get the
      new dependency
    - jna: overide the default library loading

--- platform/libs.jna/src/org/netbeans/libs/jna/Installer.java
+++ platform/libs.jna/src/org/netbeans/libs/jna/Installer.java
@@ -18,15 +18,72 @@
  */
 package org.netbeans.libs.jna;
 
+import java.io.File;
+import java.lang.System.Logger.Level;
+import java.util.Locale;
+import org.openide.modules.InstalledFileLocator;
 import org.openide.modules.ModuleInstall;
+import org.openide.util.*;
+
+import static java.lang.System.setProperty;
 
 public class Installer extends ModuleInstall {
+
+    private static final System.Logger LOG = System.getLogger(Installer.class.getName());
+
+    private static final String JNIDISPATCHNB = "jnidispatch-nb";
+    private static final String JNA_CND = "org.netbeans.libs.jna";
 
     @Override
     public void validate() {
         super.validate();
+
         //#211655
-        System.setProperty( "jna.boot.library.name", "jnidispatch-nb" ); //NOI18N
+        System.setProperty("jna.boot.library.name", JNIDISPATCHNB); //NOI18N
         System.setProperty( "jna.nosys", "false" ); //NOI18N
+        InstalledFileLocator ifl = InstalledFileLocator.getDefault();
+        String arch = System.getProperty("os.arch"); // NOI18N
+        String system = System.getProperty("os.name").toLowerCase(Locale.ENGLISH); // NOI18N
+        String mapped = System.mapLibraryName(JNIDISPATCHNB);
+
+        // See org.netbeans.StandardModule.OneModuleClassLoader#findLibrary in
+        // core.startup.base for the base idea of the logic.
+        File lib = null;
+
+        if(lib == null) {
+            lib = ifl.locate("modules/lib/" + mapped, JNA_CND, false); // NOI18N
+        }
+
+        if(lib == null) {
+            lib = ifl.locate("modules/lib/" + arch + "/" + mapped, JNA_CND, false); // NOI18N
+        }
+
+        if(lib == null) {
+            lib = ifl.locate("modules/lib/" + arch + "/" + system + "/" + mapped, JNA_CND, false); // NOI18N
+        }
+
+        if (lib == null) {
+            if (BaseUtilities.isMac()) {
+                String jniMapped = mapped.replaceFirst("\\.dylib$", ".jnilib");
+                if (lib != null) {
+                    lib = ifl.locate("modules/lib/" + jniMapped, JNA_CND, false); // NOI18N
+                }
+
+                if (lib != null) {
+                    lib = ifl.locate("modules/lib/" + arch + "/" + jniMapped, JNA_CND, false); // NOI18N
+                }
+
+                if (lib != null) {
+                    lib = ifl.locate("modules/lib/" + arch + "/" + system + "/" + jniMapped, JNA_CND, false); // NOI18N
+                }
+            }
+        }
+
+        if(lib != null) {
+            LOG.log(Level.DEBUG, "Found JNA library for OS in path: {0}", lib);
+            System.setProperty("jna.boot.library.path", lib.getAbsoluteFile().getParent()); //NOI18N
+        } else {
+            LOG.log(Level.WARNING, "Failed to find location for JNA library");
+        }
     }
 }
